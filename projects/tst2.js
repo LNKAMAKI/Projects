ace70befbae2a4cdb6e21c66b5045e410d997edf
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2021-12-16 15:39:51 -0300

    changes made

--- projects/tst2.js
+++ projects/tst2.js
@@ -2,7 +2,6 @@
         m = document.getElementById('massa').value 
         a = document.getElementById('altura').value 
         circu = document.getElementById('circ')
-        window.alert(circu)
         res = document.getElementById('result')
         if (m.length != 0 && a.length != 0) {
         console.log(m,a)
@@ -10,7 +9,7 @@
         console.log(m/(a*a))
          
         res.innerText = Math.floor(m/(a*a))
-        circu.style.opacity = '100'
+        circu.style.display = 'grid'
         }else{
             window.alert('Preencha os dados corretamente')
         }
