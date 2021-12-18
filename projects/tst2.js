9b2409170acff8746e4bb3f12049b44e78d616a7
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2021-12-17 22:15:21 -0300

    upodate

--- projects/tst2.js
+++ projects/tst2.js
@@ -4,12 +4,17 @@
         a = document.getElementById('altura').value 
        core = document.createElement('p')
        core.id = 'corres'
-       document.getElementById('bd').appendChild(core)
+
+       hd = document.getElementById('bd')
+       hd.appendChild(core)
+       
 
         e = document.getElementsByClassName('end')[0]
         if (remov != 0) {
         e.removeChild(document.getElementById('circ'))
         document.getElementById('bd').removeChild(document.getElementById('corres'))
+        hd.removeChild(document.getElementsByTagName('table')[0])
+        hd.removeChild(document.getElementById('motab'))
         }
         
         circu = document.createElement('div')
@@ -47,9 +52,59 @@
             }else{
                 core.innerText = 'Obesidade Grau III'
             }
-     
+            mt = document.createElement('input')
+            mt.setAttribute('type', 'button')
+            mt.setAttribute('class', 'button')
+            mt.value = 'Mostrar Tabela'
+            mt.style.margin = '20px auto 0px auto'
+            
+            mt.style.display = 'block'
+            mt.id = 'motab'
+
+            mt.setAttribute('onclick', 'clicado()')
+            hd.appendChild(mt)
+
+            tb = document.createElement('table')
+    tb.innerHTML = '<thead><tr><th>CLASSIFICAÇÃO</th><th>IMC</th></tr></thead><tbody><tr><td>Abaixo do Peso</td><td>Abaixo 18.5</td></tr><tr><td>Peso Normal</td><td>18.5 - 24.9</td></tr><tr><td>Sobrepeso</td><td>25 - 29.9</td></tr><tr><td>Obesidade Grau I</td><td>30 - 34.9</td></tr><tr><td>Obesidade Grau II</td><td>35 - 39.9</td></tr><tr><td>Obesidade Grau III</td><td>Maior ou Igual 40</td></tr></tbody>'
+    document.getElementById('bd').appendChild(tb)
+
         remov++
         }else{
             window.alert('Preencha os dados corretamente')
         }
     }
+
+function clicado() {
+
+    tab = document.getElementsByTagName('table')[0]
+    tab.style.display = 'table'
+
+   tab.style.border = '0px solid black'
+
+    toel = tab.getElementsByTagName('tr')
+    for (trow = 0; trow < 1; trow++) {
+
+        tdata = toel[trow].getElementsByTagName('th')
+
+        for (num = 0; num < tdata.length; num++) {
+        tdata[num].style.opacity = '0'
+        tdata[num].style.border = '0px solid black'
+        tdata[num].style.animation = 'showup 1.5s forwards'
+        }
+
+    }
+ 
+    dla = 0.5
+    for (trow = 1; trow < toel.length; trow++) {
+    tdata = toel[trow].getElementsByTagName('td')
+
+        for (num = 0; num < tdata.length; num++) {
+        tdata[num].style.opacity = '0'
+        tdata[num].style.border = '0px solid black'
+        tdata[num].style.animation = 'showup2 1.5s ' + dla + 's' + ' forwards'
+        }
+        dla += 0.5
+
+    }
+   
+}
