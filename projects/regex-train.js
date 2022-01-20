cddca82e58e4c7deb46f9933caf3509db3f8789a
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2022-01-20 15:14:04 -0300

    a

--- projects/regex-train.js
+++ projects/regex-train.js
@@ -1,14 +1,6 @@
-ex = '610.19445454544545454'
-
-ent(ex.replace(new RegExp('(?<=[0-9]\.[0-9]{2})[0-9]+'), ''))
-
-
-
-
-
-
-function ent(frase) {
-parag = document.createElement('p')
-parag.innerText = frase
-document.body.appendChild(parag)
-}
+exp = ' 12 x 10 '
+console.log(exp)
+remodelado = exp.replace(new RegExp('x','g'),'*')
+console.log(remodelado)
+console.log(remodelado.replace(new RegExp('\\s(?=[0-9]+)','g'),'Number('))
+console.log(remodelado.replace(new RegExp('\\s(?=[0-9]+)','g'),'Number('))
