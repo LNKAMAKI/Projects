a649a2d652a4a1429a0935134825f47d64e68315
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2022-02-05 21:40:58 -0300

    pq?

--- projects/celsius-fahrenheit.js
+++ projects/celsius-fahrenheit.js
@@ -62,7 +62,59 @@ function mudarVar(n) {
 
         res = ((sca2num3 - sca2num1)*(sca1num2 - sca1num1) + (sca1num3 - sca1num1)*sca2num1) / (sca1num3 - sca1num1)
 
+       sca2rel2 = document.getElementById('sca2rel2')
+       console.log(sca2rel2)
+       //sca2rel2.style.backgroundColor = 'red'
+       sca2rel2.style.top = 'calc(-99.5%)'
+
+       if (String(sca1num2).length > 0) {
+        document.getElementById('sca2num2').value = res
+        sca2rel2 = document.getElementById('sca2rel2')
+        sca2rel3 = document.getElementById('sca2rel3')
        
+        prop = 100/((sca2num3 - sca2num1)/(res - sca2num1))
+        
+        if (Number(res) < Number(sca2num1)) {
+            sca2rel2.style.top = `calc(0%)`
+            
+            prop3 = 100/((sca2num3 - sca2num2)/(sca2num1 - sca2num2))
+        
+            document.getElementById('sca2rel1').style.top = `calc(${-prop3}%)`
+    
+            sca2rel3.style.top = `calc(0%)`
+    
+        }else{
+        if (Number(res) <= Number(sca2num3)) {
+           if (prop != '100') {
+            sca2rel2.style.top = `calc(${-prop}%)`
+           }else{
+            sca2rel2.style.top = `calc(${-99.5}%)`
+           }
+        
+           prop3 = 100/((sca2num3 - sca2num1)/(sca2num1 - sca2num1))
+        
+        document.getElementById('sca2rel1').style.top = `calc(${-prop3}%)`
+    
+        sca2rel3.style.top = `calc(0%)`
+    
+        prop3 = 100/((sca2num3 - sca2num1)/(sca2num1 - sca2num1))
+        
+        document.getElementById('sca2rel1').style.top = `calc(${-prop3}%)`
+        }else{
+            sca2rel2.style.top = `calc(-99.5%)`
+           
+            prop2 = 100/((sca2num2 - sca2num1)/(sca2num3 - sca2num1))
+            sca2rel3.style.top = `calc(${100 - prop2}%)`
+            
+            prop3 = 100/((sca2num3 - sca2num1)/(sca2num1 - sca2num1))
+        
+        document.getElementById('sca2rel1').style.top = `calc(${-prop3}%)`
+        }
+    }
+     
+        }else{
+            document.getElementById('sca2num2').value = ''
+        }
 
     }else{
         res = ((sca1num3 - sca1num1)*(sca2num2 - sca2num1) + (sca2num3 - sca2num1)*sca1num1) / (sca2num3 - sca2num1)
