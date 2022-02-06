83484fd2bdce2c69a162a262dae28bbc0223363e
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2022-02-06 15:30:52 -0300

    aaa

--- projects/celsius-fahrenheit.js
+++ projects/celsius-fahrenheit.js
@@ -58,72 +58,82 @@ function mudarVar(n) {
     sca2num2 = document.getElementById('sca2num2').value
     sca2num3 = document.getElementById('sca2num3').value
 
+    formula = document.getElementById('form')
+    titfor = document.getElementById('titfor')
+
+    fstspec = document.getElementById('fstspec').value
+    secspec = document.getElementById('secspec').value
+
+
+    titfor.style.color = 'black'
+    console.log(fstspec, secspec)
+    formula.innerHTML = `(${fstspec} - ${sca2num1})/${sca2num3 - sca2num1} = (${secspec} - ${sca1num1})/${sca1num3 - sca1num1}`
+
+
     if (n == 1) {
 
         res = ((sca2num3 - sca2num1)*(sca1num2 - sca1num1) + (sca1num3 - sca1num1)*sca2num1) / (sca1num3 - sca1num1)
 
-        /*
-        sca2rel2 = document.getElementById('sca2rel2')
-        console.log(sca2rel2)
-        //sca2rel2.style.backgroundColor = 'red'
-        sca2rel2.style.top = 'calc(-99.5%)'
- 
-        
         if (String(sca1num2).length > 0) {
-         document.getElementById('sca2num2').value = res
-         sca2rel2 = document.getElementById('sca2rel2')
-         sca2rel3 = document.getElementById('sca2rel3')
-        
-         prop = 100/((sca2num3 - sca2num1)/(res - sca2num1))
-         
-         if (Number(res) < Number(sca2num1)) {
-             sca2rel2.style.top = `calc(0%)`
-             
-             prop3 = 100/((sca2num3 - sca2num2)/(sca2num1 - sca2num2))
-         
-             document.getElementById('sca2rel1').style.top = `calc(${-prop3}%)`
-     
-             sca2rel3.style.top = `calc(0%)`
-     
-         }else{
-         if (Number(res) <= Number(sca2num3)) {
-            if (prop != '100') {
-                if (Number(prop) <= 99.5) {
-             sca2rel2.style.top = `calc(${-prop}%)`
-                }
-            }else{
-             sca2rel2.style.top = `calc(${-99.5}%)`
-            }
-         
-            prop3 = 100/((sca2num3 - sca2num1)/(sca2num1 - sca2num1))
-         
-         document.getElementById('sca2rel1').style.top = `calc(${-prop3}%)`
-     
-         sca2rel3.style.top = `calc(0%)`
-     
-         prop3 = 100/((sca2num3 - sca2num1)/(sca2num1 - sca2num1))
-         
-         document.getElementById('sca2rel1').style.top = `calc(${-prop3}%)`
-         }else{
-             sca2rel2.style.top = `calc(-99.5%)`
+            document.getElementById('sca2num2').value = res
+            sca2rel2 = document.getElementById('sca2rel2')
+            sca2rel3 = document.getElementById('sca2rel3')
+           
+            prop = 100/((sca2num3 - sca2num1)/(res - sca2num1))
             
-             prop2 = 100/((sca2num2 - sca2num1)/(sca2num3 - sca2num1))
-
-             if (Number(prop2) <= 100) {
-             sca2rel3.style.top = `calc(${100 - prop2}%)`
-             }
-
-             prop3 = 100/((sca2num3 - sca2num1)/(sca2num1 - sca2num1))
+            if (Number(res) < Number(sca2num1)) {
+                sca2rel2.style.top = `calc(0%)`
+                
+                prop3 = 100/((sca2num3 - sca2num2)/(sca2num1 - sca2num2))
+            
+                if (Number(prop3) >= 0) {
+                document.getElementById('sca2rel1').style.top = `calc(${-prop3}%)`
+                }
+                sca2rel3.style.top = `calc(0%)`
+        
+            }else{
+            if (Number(res) <= Number(sca2num3)) {
+               if (prop != '100') {
+                   if (Number(prop) <= 99.5) {
+                sca2rel2.style.top = `calc(${-prop}%)`
+                   }else{
+                       sca2rel2.style.backgroundColor = 'red'
+                   }
+               }else{
+                sca2rel2.style.top = `calc(${-99.5}%)`
+               }
+            
+               prop3 = 100/((sca2num3 - sca2num1)/(sca2num1 - sca2num1))
+            
+               if (Number(prop3) >= 0) {
+            document.getElementById('sca2rel1').style.top = `calc(${-prop3}%)`
+               }
+            sca2rel3.style.top = `calc(0%)`
+        
+            prop3 = 100/((sca2num3 - sca2num1)/(sca2num1 - sca2num1))
+            
+            if (Number(prop3) >= 0) {
+            document.getElementById('sca2rel1').style.top = `calc(${-prop3}%)`
+            }
+            }else{
+                sca2rel2.style.top = `calc(-99.5%)`
+               
+                prop2 = 100/((sca2num2 - sca2num1)/(sca2num3 - sca2num1))
+                if (Number(prop2) <= 100 && Number(prop2) >= 0) {
+                sca2rel3.style.top = `calc(${100 - prop2}%)`
+                }
+                
+                prop3 = 100/((sca2num3 - sca2num1)/(sca2num1 - sca2num1))
+            
+                if (Number(prop3) >= 0) {
+            document.getElementById('sca2rel1').style.top = `calc(${-prop3}%)`
+                }
+            }
+        }
          
-         document.getElementById('sca2rel1').style.top = `calc(${-prop3}%)`
-         }
-     }
-      
-         }else{
-             document.getElementById('sca2num2').value = ''
-             sca2rel1.style.top = 'calc(0%)'
-         }
- */
+            }else{
+                document.getElementById('sca2num2').value = ''
+            }
     }else{
         res = ((sca1num3 - sca1num1)*(sca2num2 - sca2num1) + (sca2num3 - sca2num1)*sca1num1) / (sca2num3 - sca2num1)
    
--- projects/empty.js
+++ projects/empty.js
@@ -10,36 +10,48 @@ if (String(sca1num2).length > 0) {
         
         prop3 = 100/((sca2num3 - sca2num2)/(sca2num1 - sca2num2))
     
+        if (Number(prop3) >= 0) {
         document.getElementById('sca2rel1').style.top = `calc(${-prop3}%)`
-
+        }
         sca2rel3.style.top = `calc(0%)`
 
     }else{
     if (Number(res) <= Number(sca2num3)) {
        if (prop != '100') {
+           if (Number(prop) <= 99.5) {
         sca2rel2.style.top = `calc(${-prop}%)`
+           }else{
+               sca2rel2.style.backgroundColor = 'red'
+           }
        }else{
         sca2rel2.style.top = `calc(${-99.5}%)`
        }
     
        prop3 = 100/((sca2num3 - sca2num1)/(sca2num1 - sca2num1))
     
+       if (Number(prop3) >= 0) {
     document.getElementById('sca2rel1').style.top = `calc(${-prop3}%)`
-
+       }
     sca2rel3.style.top = `calc(0%)`
 
     prop3 = 100/((sca2num3 - sca2num1)/(sca2num1 - sca2num1))
     
+    if (Number(prop3) >= 0) {
     document.getElementById('sca2rel1').style.top = `calc(${-prop3}%)`
+    }
     }else{
         sca2rel2.style.top = `calc(-99.5%)`
        
         prop2 = 100/((sca2num2 - sca2num1)/(sca2num3 - sca2num1))
+        if (Number(prop2) <= 100 && Number(prop2) >= 0) {
         sca2rel3.style.top = `calc(${100 - prop2}%)`
+        }
         
         prop3 = 100/((sca2num3 - sca2num1)/(sca2num1 - sca2num1))
     
+        if (Number(prop3) >= 0) {
     document.getElementById('sca2rel1').style.top = `calc(${-prop3}%)`
+        }
     }
 }
  
