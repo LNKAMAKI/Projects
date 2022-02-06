4f9ddb0b33add1dd30760181d0dab0a56ff7a14f
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2022-02-06 11:56:54 -0300

    Update celsius-fahrenheit.js

--- projects/celsius-fahrenheit.js
+++ projects/celsius-fahrenheit.js
@@ -139,8 +139,9 @@ function mudarVar(n) {
                 
                 prop3 = 100/((sca1num3 - sca1num2)/(sca1num1 - sca1num2))
             
+                if (Number(prop3) >= 0) {
                 document.getElementById('sca1rel1').style.top = `calc(${-prop3}%)`
-
+                }
                 sca1rel3.style.top = `calc(0%)`
 
             }else{
@@ -157,24 +158,29 @@ function mudarVar(n) {
             
                prop3 = 100/((sca1num3 - sca1num1)/(sca1num1 - sca1num1))
             
+               if (Number(prop3) >= 0) {
             document.getElementById('sca1rel1').style.top = `calc(${-prop3}%)`
-
+               }
             sca1rel3.style.top = `calc(0%)`
 
             prop3 = 100/((sca1num3 - sca1num1)/(sca1num1 - sca1num1))
             
+            if (Number(prop3) >= 0) {
             document.getElementById('sca1rel1').style.top = `calc(${-prop3}%)`
+            }
             }else{
                 sca1rel2.style.top = `calc(-99.5%)`
                
                 prop2 = 100/((sca1num2 - sca1num1)/(sca1num3 - sca1num1))
-                if (Number(prop2) <= 100) {
+                if (Number(prop2) <= 100 && Number(prop2) >= 0) {
                 sca1rel3.style.top = `calc(${100 - prop2}%)`
                 }
                 
                 prop3 = 100/((sca1num3 - sca1num1)/(sca1num1 - sca1num1))
             
+                if (Number(prop3) >= 0) {
             document.getElementById('sca1rel1').style.top = `calc(${-prop3}%)`
+                }
             }
         }
          
