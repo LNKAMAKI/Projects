824bb25fe82dcfc532d4e61f378646e5b54a1029
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2022-02-23 12:48:47 -0300

    Update regex-train.js

--- projects/regex-train.js
+++ projects/regex-train.js
@@ -1,1 +1,15 @@
+expression = ['1', '+', '2', 'x', '3', '+', '9']
+console.log(expression, expression.length) 
 
+for (n = 0; n < expression.length; n++) {
+    console.log(expression[n])
+    if (expression[n] == 'x') {
+        console.log('Multiplicar')
+        console.log(expression[n - 1]*expression[n + 1])
+        expression[n + 1] = expression[n - 1]*expression[n + 1]
+
+    }
+}
+
+
+console.log(expression)
