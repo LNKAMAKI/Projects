e78f9b8bcb7fb7c8dfbf53a68d290ecb7fd5849d
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2022-02-25 11:03:46 -0300

    SAVE

--- projects/empty.js
+++ projects/empty.js
@@ -1,6 +1,39 @@
-array = []
-variable = 1
-variable2 = 10
-array.push({})
-array[0].id = variable
-array[0].number = variable2
+numbers = [{number: 0}, {number: 1}, {number: 0}]
+console.log(sort(numbers))
+
+function sort(n) {
+    var maiorparamenor = []
+    var posiçõesnumeros = []
+    var yposition = n
+
+    for (n in yposition) {
+        maiorparamenor.push({ par:-100000000000000000000000000000000000000000000000000000000000000000000000000000000})
+        posiçõesnumeros.push(-1)
+
+    }
+
+    for (item in yposition) {
+        var volume = 0
+        var dn = 0
+        for (volume in yposition) {
+            if (dn == 0) {
+                if (yposition[item].par > maiorparamenor[volume].par) {
+                    var itens = yposition.length - (Number(volume) + 1)
+                    var leng = yposition.length
+
+                    for (c = 0; c < itens; c++) {
+                        maiorparamenor[Number(leng) - 1] = maiorparamenor[Number(leng) - 2]
+                        posiçõesnumeros[Number(leng) - 1] = posiçõesnumeros[Number(leng) - 2]
+                        leng--
+                    }
+                    maiorparamenor[volume] = yposition[item]
+                    posiçõesnumeros[volume] = Number(item)
+                    dn = 10
+
+
+                }
+            }
+        }
+    }
+    return [maiorparamenor, posiçõesnumeros]
+}
--- projects/regex-train.js
+++ projects/regex-train.js
@@ -93,3 +93,46 @@ for (e = 0; e < expression.length; e++) {
         parents++
     }
 }
+
+degSort = sort(degs)[0]
+console.log(degSort)
+
+function sort(n) {
+    var maiorparamenor = []
+    var posiçõesnumeros = []
+    var yposition = n
+
+    for (n in yposition) {
+        maiorparamenor.push({ par:-100000000000000000000000000000000000000000000000000000000000000000000000000000000})
+        posiçõesnumeros.push(-1)
+
+    }
+
+    for (item in yposition) {
+        var volume = 0
+        var dn = 0
+        for (volume in yposition) {
+            if (dn == 0) {
+                if (yposition[item].par > maiorparamenor[volume].par) {
+                    var itens = yposition.length - (Number(volume) + 1)
+                    var leng = yposition.length
+
+                    for (c = 0; c < itens; c++) {
+                        maiorparamenor[Number(leng) - 1] = maiorparamenor[Number(leng) - 2]
+                        posiçõesnumeros[Number(leng) - 1] = posiçõesnumeros[Number(leng) - 2]
+                        leng--
+                    }
+                    maiorparamenor[volume] = yposition[item]
+                    posiçõesnumeros[volume] = Number(item)
+                    dn = 10
+
+
+                }
+            }
+        }
+    }
+    return [maiorparamenor, posiçõesnumeros]
+}
+
+    
+        
