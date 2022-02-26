39db4ac88c4ec62fa31d7c5b975e9157fb461cea
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2022-02-26 19:22:31 -0300

    Save

--- projects/calculadora.js
+++ projects/calculadora.js
@@ -3,7 +3,7 @@ expression = ['(','(','10', '+', '(','50', '+', '100', '+', '(','(','200', '+', 
 '(', '30', '+', '(', '150', '+', '(', '20', '+', '(', '110', '+', '50',')', '+', '(','40', '+', '2',')',
 ')',')',')',')',')', '+', '60', '+', '6', '+', '(', '10', '+', '7', ')', ')', '+', '100', ')', '+','500']
 
-expression = [ '20', '/', '(', '2', 'x', '5', ')', '+', '(', '8', '+', '7', '+', '3', ')']
+expression = ['64','r','3']
 
 console.log(expression, expression.length) 
 stop = false
@@ -48,6 +48,23 @@ if(expression[n] == 'x' && expression[n - 1] != '(' && expression[n - 1] != ')' 
 }else if(expression[n] == '^' && expression[n - 1] != '(' && expression[n - 1] != ')' && expression[n + 1] != '(' && expression[n + 1] != ')') {
     console.log('DIVISÃO')
     expression[n + 1] = Number(expression[n - 1]) ** Number(expression[n + 1])
+    expression.splice(n - 1, 2)
+    
+    n--
+    
+    if (expression[n - 1] == '(' && expression[n + 1] == ')') {
+        console.log('TIRAR')
+        expression.splice(n + 1, 1)
+        expression.splice(n - 1, 1)
+        n = n - 3
+
+        console.log(expression)
+    }else{
+        console.log(expression)
+    }
+}else if(expression[n] == 'r' && expression[n - 1] != '(' && expression[n - 1] != ')' && expression[n + 1] != '(' && expression[n + 1] != ')') {
+    console.log('RAIZ')
+    expression[n + 1] = Number(expression[n - 1]) ** (1/ Number(expression[n + 1]))
     expression.splice(n - 1, 2)
     
     n--
