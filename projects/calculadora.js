63e39b25d0786dcd1efc88691296f4d9538f8cea
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2022-02-27 11:11:31 -0300

    SAVE

--- projects/calculadora.js
+++ projects/calculadora.js
@@ -4,36 +4,69 @@ window.addEventListener('keydown', (event)=> {
     if (event.key != 'Shift'){
     console.log(event.key)
     
-    if (String(event.key).search('[0-9]') != -1 || event.key.search('[\\.\\(\\)\\+\\-\\x\\/r]') != -1) {
+    if (String(event.key).search('[0-9]') != -1 || event.key.search('[\\.\\(\\)\\+\\-\\x\\/r**=]') != -1) {
         if (numero.length > 0) {
             if (String(event.key).search('\\.') != -1 || String(event.key).search('[0-9]') != -1) {
                 if (String(numero[numero.length - 1]).search('\\.') != -1 || String(numero[numero.length - 1]).search('[0-9]') != -1) {
-                console.log('AAAAAAAAAAAAAAAAAAAA' + numero[numero.length - 1].search('[0-9]'))
-                console.log('DO IT')
-            numero+= event.key
-                }else{
-                    console.log('TIRAR') 
+                    console.log('AAAAAAAAAAAAAAAAAAAA' + numero[numero.length - 1].search('[0-9]'))
+                    console.log('DO IT')
+                    numero += String(event.key)
+                } else {
+                    console.log('TIRAR')
                     expression.push(numero)
-                    numero = event.key
+                    console.log(expression)
+                    numero = String(event.key)
                 }
-            }else{
+            } else {
                 console.log('AAAAAAAAAAAAAAAAAAAA' + numero[numero.length - 1].search('\\.'))
                 console.log('RETIRAR')
                 expression.push(numero)
-                numero = event.key
+                console.log(expression)
+                numero = String(event.key)
             }
-          
-        }else{
+    
+        } else {
             console.log('NUMERO VAZIO')
-            numero+= event.key
+            numero += String(event.key)
         }
-
+        console.log(numero)
+    
     }
+    
 }
 })
 function inserir(n) {
+    console.log(n)
+    if (String(n).search('[0-9]') != -1 || n.search('[\\.\\(\\)\\+\\-\\x\\/r**=]') != -1) {
+        if (numero.length > 0) {
+            if (String(n).search('\\.') != -1 || String(n).search('[0-9]') != -1) {
+                if (String(numero[numero.length - 1]).search('\\.') != -1 || String(numero[numero.length - 1]).search('[0-9]') != -1) {
+                    console.log('AAAAAAAAAAAAAAAAAAAA' + numero[numero.length - 1].search('[0-9]'))
+                    console.log('DO IT')
+                    numero += String(n)
+                } else {
+                    console.log('TIRAR')
+                    expression.push(numero)
+                    console.log(expression)
+                    numero = String(n)
+                }
+            } else {
+                console.log('AAAAAAAAAAAAAAAAAAAA' + numero[numero.length - 1].search('\\.'))
+                console.log('RETIRAR')
+                expression.push(numero)
+                console.log(expression)
+                numero = String(n)
+            }
 
+        } else {
+            console.log('NUMERO VAZIO')
+            numero += String(n)
+        }
+        console.log(numero)
+
+    }
 }
+
 
 function fazerConta() {
 for (n = 0; n < expression.length; n++) {
