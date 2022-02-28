105f680111b671c10b04041a6b53732b315daa2b
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2022-02-28 16:08:22 -0300

    Update calculadora.js

--- projects/calculadora.js
+++ projects/calculadora.js
@@ -175,12 +175,13 @@ function fazerConta(anterior) {
         console.log('RAIZ')
         expression[n + 1] = String(Number(expression[n + 1]) ** (1/2))
         expression.splice(n, 1)
-        
+        console.log(n)
         n = n - 2
-        if (expression[n - 1] == '(' && expression[n + 1] == ')') {
+        console.log(n)
+        if (expression[n + 1] == '(' && expression[n + 3] == ')') {
             console.log('TIRAR')
+            expression.splice(n + 3, 1)
             expression.splice(n + 1, 1)
-            expression.splice(n - 1, 1)
             n = n - 3
     
             console.log(expression)
@@ -325,10 +326,11 @@ if (expression.length > 1) {
                 expression.splice(n, 1)
                 
                 n = n - 2
-                if (expression[n - 1] == '(' && expression[n + 1] == ')') {
+                console.log(n)
+                if (expression[n + 1] == '(' && expression[n + 3] == ')') {
                     console.log('TIRAR')
+                    expression.splice(n + 3, 1)
                     expression.splice(n + 1, 1)
-                    expression.splice(n - 1, 1)
                     n = n - 3
             
                     console.log(expression)
