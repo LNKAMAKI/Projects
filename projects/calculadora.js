b9889c0e68b4ecb0682b14cf06b013d16bab0b4c
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2022-02-28 12:53:50 -0300

    Update calculadora.js

--- projects/calculadora.js
+++ projects/calculadora.js
@@ -3,93 +3,7 @@ expression = []
 
 window.addEventListener('keydown', (event)=> {
     if (String(event.key).search('[0-9]') != -1 || event.key.search('[\\.\\(\\)\\+\\-\\x\\/r*=Backspace]') != -1) {
-        console.log(event.key)
-        console.log('DIGITO: ' + event.key)
-    if (String(event.key).search('[Enter=Backspace]') == -1) {
-    if (ansd == false) {
-    if (expression.length > 0) {
-
-    if (String(event.key).search('[0-9]') != -1 || String(event.key) == '.') {
-        console.log('NUMERO OU PONTO')
-
-        if (ansd == true) {
-            ansd = false
-        }
-       if (String(expression[expression.length - 1]).search('[0-9]') != -1 || String(expression[expression.length - 1]) == '.') {
-        console.log(expression[expression.length - 1] + ' é um número ou ponto')
-        expression[position] += String(event.key)
-       }else{
-           console.log(expression[expression.length - 1] + ' não é um número ou ponto')
-           position++
-        expression[position] = String(event.key)
-       }
-    }else{
-        position++
-        expression[position] = String(event.key)
-    }
-  
-}else{
-    console.log('LISTA VAZIA')
-   expression.push(String(event.key))
-  
-}
-console.log(expression)
-console.log('POSITION: ' + position)
-console.log(' ')
-    }else{
-        console.log('ansd = true')
-        if (String(event.key).search('[0-9]') != -1 || String(event.key).search('[\\.\\(\\)]') != -1) {
-            expression[0] = event.key
-        }else{
-       position++
-        expression[position] = event.key
-    }
-    ansd = false
-}
-    }else{
-        if (String(event.key) == 'Backspace') {
-            console.log('Backspace')
-           
-            if (String(expression[expression.length - 1]).length == 1) {
-                console.log(expression[expression.length - 1] + ' tem tamanho 1')
-                expression.splice(expression.length - 1, 1)
-                console.log(expression)
-
-                if (position > 0) {
-                    position--
-                    }
-            }else{
-                slicednum = ''
-                console.log(expression[expression.length - 1] + ' tem tamanho maior que 1') 
-                for (na = 0; na <= Number(String(expression[expression.length - 1]).length) - 2; na++) {
-                    console.log(na, String(expression[expression.length - 1])[na])
-                    slicednum +=  String(expression[expression.length - 1])[na]
-                }
-                console.log(slicednum)
-                expression[expression.length - 1] = slicednum
-                console.log(expression)
-
-            }
-        }else{
-            console.log('=')
-            if (expression.length != 0) {
-                anstxt = txt
-            fazerConta(anstxt)
-            position = 0
-            ansd = true
-            }
-        }
-    }
-    txt = ''
-    for (b = 0; b < expression.length; b++) {
-        console.log(expression[b])
-        txt+= ` ${expression[b]}`
-    }
-    console.log(txt)
-    if (event.key != '=' && event.key != 'Enter') {
-    document.getElementById('put').innerText = txt 
-    }
-
+    
     }
 })
 
@@ -143,8 +57,10 @@ function inserir(n) {
     }
   
 }else{
-    //console.log('LISTA VAZIA')
+    console.log('LISTA VAZIA')
+    if (String(n).search('[0-9]') != -1 || String(n) == '.' || String(n) == '(' || String(n) == '-')  {
    expression.push(String(n))
+    }
    if (n == '-') {
     console.log('OOOPS')
     juntar = true
@@ -157,25 +73,27 @@ console.log(expression)
     }else{
         console.log('ansd = true')
        
-        if (String(n).search('[0-9]') != -1 || String(n) == '.' || String(n).search('[\\(\\)]') != -1) {
+        if (String(n).search('[0-9]') != -1 || String(n) == '.' || String(n) == '(') {
             expression[0] = n
-            
-        }else{
+            ansd = false
+        }else if (document.getElementById('put').innerText != 'Error' && String(n) != ')'){
        position++
         expression[position] = n
+        ansd = false
     }
-    ansd = false
+    
 }
     }else{
         if (String(n) == 'CE') {
           //  console.log('CE')
            
             if (String(expression[expression.length - 1]).length == 1) {
+                console.log('AAAAAAAA')
                // console.log(expression[expression.length - 1] + ' tem tamanho 1')
                 expression.splice(expression.length - 1, 1)
                 //console.log(expression)
 
-                if (position > 0) {
+                if (position != 0) {
                     position--
                     }
             }else{
@@ -193,6 +111,11 @@ console.log(expression)
             console.log(expression)
             if (expression.length == 0) {
               //  document.getElementById('ans').innerText = ''
+            }
+
+            if (expression.length == 0) {
+                console.log('É ZEEEEEEEEEEEEEEROOOOOOOOOOOOOO')
+                ansd = false
             }
         }else{
            // console.log('=')
@@ -433,5 +356,6 @@ if (expression.length > 1) {
     console.log('what?')
     expression = []
     document.getElementById('put').innerText = 'Error'
+    
 }
 }
