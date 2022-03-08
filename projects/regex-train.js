1198bb316d83c1ebaaabc9c2845b7cbc9ab12b4d
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2022-03-07 22:40:34 -0300

    Update regex-train.js

--- projects/regex-train.js
+++ projects/regex-train.js
@@ -1,4 +1,39 @@
-console.log('OLÁ')
+number = [{o: 1},{o: 2},{o: 6},{o: 4},{o: 3}]
+console.log(sort(number, 'o'))
 
-r = [{num: 'xx'}, {num: 'x'}]
+function sort(n, pam) {
+    var maiorparamenor = []
+    var posiçõesnumeros = []
+    var yposition = n
 
+    for (n in yposition) {
+        maiorparamenor.push(-100000000000000000000000000000000000000000000000000000000000000000000000000000000)
+        posiçõesnumeros.push(-1)
+
+    }
+
+    for (item in yposition) {
+        var volume = 0
+        var dn = 0
+        for (volume in yposition) {
+            if (dn == 0) {
+                if (yposition[item][pam] > maiorparamenor[volume]) {
+                    var itens = yposition.length - (Number(volume) + 1)
+                    var leng = yposition.length
+
+                    for (c = 0; c < itens; c++) {
+                        maiorparamenor[Number(leng) - 1] = maiorparamenor[Number(leng) - 2]
+                        posiçõesnumeros[Number(leng) - 1] = posiçõesnumeros[Number(leng) - 2]
+                        leng--
+                    }
+                    maiorparamenor[volume] = yposition[item][pam]
+                    posiçõesnumeros[volume] = Number(item)
+                    dn = 10
+
+
+                }
+            }
+        }
+    }
+    return [maiorparamenor, posiçõesnumeros]
+}
