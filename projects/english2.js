696d2ceddf5bdf337a99dfee9d8249621d5a7f12
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2022-04-27 13:04:36 -0300

    A

--- projects/english2.js
+++ projects/english2.js
@@ -11,27 +11,23 @@ console.log(hat)
 console.log(hat[ident])
 hat[ident].style.animation = 'slide3 0.6s cubic-bezier(0.4, 0, 0, 0.39) forwards'
 
-ident++
+if (ident == 0) {
+ident = 1
+}else if(ident == 1) {
+   ident = 0
+}
 qual++
-outra = document.createElement('div')
-outra.setAttribute('class', 'scroll')
-outra.id = ident
+outra = hat[ident]
 outra.style.visibility = 'visible'
 
-switch (qual) {
-   case 0: 
-   outra.innerHTML = '<div class="pic"><img src="HTML symbol.webp" alt=""></div>'
-   break
-   case 1: 
-   outra.innerHTML = '<div class="pic"><img src="css-logo.webp" alt=""></div></div>'
-   break
-   case 2: 
-   outra.innerHTML = '<div class="pic"><img src="hello world.gif" alt=""></div>'
-   break
-}
+
+sw(qual)
+
 outra.style.transform = 'translate(200%)'
 outra.style.animation = 'slide2 0.6s cubic-bezier(0.4, 0, 0, 0.39) forwards'
-document.getElementById('seila').appendChild(outra)
+
+
+
 
 
 
@@ -39,38 +35,64 @@ document.getElementById('seila').appendChild(outra)
 
 
 function prev() {
+   if (qual > 0) {
    hat = document.getElementsByClassName('scroll')
+
 console.log(hat)
 
 
+hat[ident].style.animation = 'slide3 0.6s cubic-bezier(0.4, 0, 0, 0.39) forwards'
 
-console.log(hat[ident])
-hat[ident].style.animation = 'slide 0.6s cubic-bezier(0.4, 0, 0, 0.39) forwards'
 
-ident++
+if (ident == 0) {
+ident = 1
+}else if(ident == 1) {
+   ident = 0
+}
 qual--
-outra = document.createElement('div')
-outra.setAttribute('class', 'scroll')
-outra.id = ident
+outra = hat[ident]
 outra.style.visibility = 'visible'
 
-switch (qual) {
-   case 0: 
-   outra.innerHTML = '<div class="pic"><img src="HTML symbol.webp" alt=""></div>'
-   break
-   case 1: 
-   outra.innerHTML = '<div class="pic"><img src="css-logo.webp" alt=""></div></div>'
-   break
-   case 2: 
-   outra.innerHTML = '<div class="pic"><img src="hello world.gif" alt=""></div>'
-   break
-}
-outra.style.transform = 'translate(-200%)'
+sw(qual)
+
+outra.style.transform = 'translate(200%)'
 outra.style.animation = 'slide2 0.6s cubic-bezier(0.4, 0, 0, 0.39) forwards'
-document.getElementById('seila').appendChild(outra)
+   }
 
 
 
+}
+
+function sw(what){
+   document.getElementById('p').parentNode.removeChild(document.getElementById('p'))
+   cont = document.createElement('p')
+   cont.id = 'p'
+   document.getElementById('hold').appendChild(cont)
+   cont.style.opacity = '0'
+   cont.style.transform = 'translateY(100%)'
+   cont.style.animation = 'showup 0.5s 0.1s forwards'
+   
+   switch (what) {
+      case 0: 
+      outra.innerHTML = '<div class="pic"><img src="imagens/html-css-js.jpg" alt=""></div>'
+     cont.innerText =  'MOST SITES USE THESE THREE MAIN LANGUAGES: HTML, CSS AND JAVASCRIPT.'
+      break
+      case 1: 
+      outra.innerHTML = '<div class="pic"><img src="html-symbol.webp" alt=""></div></div>'
+      cont.innerText =  'sss'
+      break
+      case 2: 
+      outra.innerHTML = '<div class="pic"><img src="css-logo.webp" alt=""></div></div>'
+      cont.innerText =  'aaaaaaaaaaa'
+      break
+      case 3: 
+      outra.innerHTML = '<div class="pic"><img src="imagens/standard_js.png" alt=""></div></div>'
+      cont.innerText =  'aaaaaaaaaaa'
+      break
+      default: 
+      outra.innerHTML = ''
+      break
+   }
 }
 /*
                   <div class="scroll"><div class="pic"><img src="hello world.gif" alt=""></div></div>
