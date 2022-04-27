e23ddfe89ab24224cd816e33c0bbb6b4f8fc4270
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2022-04-27 06:22:16 -0300

    e

--- projects/english2.js
+++ projects/english2.js
@@ -1,14 +1,37 @@
 
 ident = 0
+
 function clicar() {
    hat = document.getElementsByClassName('scroll')
 console.log(hat)
+
+
 ident++
 console.log(hat[ident - 1])
-hat[ident - 1].style.animation = 'slide 0.5s cubic-bezier(0.4, 0, 0, 0.39) forwards'
+hat[ident - 1].style.animation = 'slide 0.6s cubic-bezier(0.4, 0, 0, 0.39) forwards'
 outra = document.createElement('div')
 outra.setAttribute('class', 'scroll')
 outra.id = ident
-document.getElementById('quests').appendChild(outra)
+outra.style.visibility = 'visible'
+
+switch (ident) {
+   case 0: 
+   outra.innerHTML = '<div class="pic"><img src="HTML symbol.webp" alt=""></div>'
+   break
+   case 1: 
+   outra.innerHTML = '<div class="pic"><img src="hello world.gif" alt=""></div>'
 
 }
+outra.style.transform = 'translate(200%)'
+outra.style.animation = 'slide2 0.6s cubic-bezier(0.4, 0, 0, 0.39) forwards'
+document.getElementById('seila').appendChild(outra)
+
+
+
+}
+
+
+
+/*
+                  <div class="scroll"><div class="pic"><img src="hello world.gif" alt=""></div></div>
+                  */
