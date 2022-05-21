ad3d20b6df4ca135ab275334b607725f63736155
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2022-05-21 15:28:24 -0300

    added number sort

--- projects/fatorador.js
+++ projects/fatorador.js
@@ -4976,3 +4976,41 @@ function MULTIPLICARDIREITO(num1, num2) {
     
 }
 console.log(FATORARSInGULAR('10x'))
+
+
+function sort(n) {
+    var maiorparamenor = []
+    var posiçõesnumeros = []
+    var yposition = n
+
+    for (n in yposition) {
+        maiorparamenor.push(-100000000000000000000000000000000000000000000000000000000000000000000000000000000)
+        posiçõesnumeros.push(-1)
+
+    }
+
+    for (item in yposition) {
+        var volume = 0
+        var dn = 0
+        for (volume in yposition) {
+            if (dn == 0) {
+                if (yposition[item] > maiorparamenor[volume]) {
+                    var itens = yposition.length - (Number(volume) + 1)
+                    var leng = yposition.length
+
+                    for (c = 0; c < itens; c++) {
+                        maiorparamenor[Number(leng) - 1] = maiorparamenor[Number(leng) - 2]
+                        posiçõesnumeros[Number(leng) - 1] = posiçõesnumeros[Number(leng) - 2]
+                        leng--
+                    }
+                    maiorparamenor[volume] = yposition[item]
+                    posiçõesnumeros[volume] = Number(item)
+                    dn = 10
+
+
+                }
+            }
+        }
+    }
+    return [maiorparamenor, posiçõesnumeros]
+}
