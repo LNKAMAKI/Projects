35ee4c87d9279346e005ff9c1926538a43abc1ee
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2022-05-21 15:27:21 -0300

    when it feels good

--- projects/ordenar-objeto.js
+++ projects/ordenar-objeto.js
@@ -1,5 +1,5 @@
-number = [{o: 1},{o: 2},{o: 6},{o: 4},{o: 3}]
-console.log(sort(number, 'o'))
+number = [{o: 1}, {o: 2}, {o: 3}, {o: 4}, {o: 5}]
+console.log(sortob(number, 'o'))
 
 function sortob(n, pam) {
     var maiorparamenor = []
@@ -27,6 +27,45 @@ function sortob(n, pam) {
                         leng--
                     }
                     maiorparamenor[volume] = yposition[item][pam]
+                    posiçõesnumeros[volume] = Number(item)
+                    dn = 10
+
+
+                }
+            }
+        }
+    }
+    return [maiorparamenor, posiçõesnumeros]
+}
+
+
+
+function sort(n) {
+    var maiorparamenor = []
+    var posiçõesnumeros = []
+    var yposition = n
+
+    for (n in yposition) {
+        maiorparamenor.push(-100000000000000000000000000000000000000000000000000000000000000000000000000000000)
+        posiçõesnumeros.push(-1)
+
+    }
+
+    for (item in yposition) {
+        var volume = 0
+        var dn = 0
+        for (volume in yposition) {
+            if (dn == 0) {
+                if (yposition[item] > maiorparamenor[volume]) {
+                    var itens = yposition.length - (Number(volume) + 1)
+                    var leng = yposition.length
+
+                    for (c = 0; c < itens; c++) {
+                        maiorparamenor[Number(leng) - 1] = maiorparamenor[Number(leng) - 2]
+                        posiçõesnumeros[Number(leng) - 1] = posiçõesnumeros[Number(leng) - 2]
+                        leng--
+                    }
+                    maiorparamenor[volume] = yposition[item]
                     posiçõesnumeros[volume] = Number(item)
                     dn = 10
 
