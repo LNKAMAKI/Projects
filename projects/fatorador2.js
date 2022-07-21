b8d9489e3303ddbf305657620b96e7c9c732c35a
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2022-07-21 18:28:21 -0300

    TÁ DANDO CERTO AQUI HEIN?

--- projects/fatorador2.js
+++ projects/fatorador2.js
@@ -3400,23 +3400,27 @@ for (bye in monomios) {
 
        //console.log('__________________')
        //console.log(naonao,',', tobreak)
-       fat1 = FATORAR2(FATORE(naonao))
-       fat2 = FATORAR2(FATORE(tobreak))
+       //fat1 = FATORAR2(FATORE(naonao))
+       //fat2 = FATORAR2(FATORE(tobreak))
        //console.log(fat1[0],'segs.length:',fat1[3])
-       //console.log(fat2[0],'segs.length:',fat2[3])
+      // console.log(fat2[0],'segs.length:',fat2[3])
    
        daprafat1 = false
+       /*
        if (fat1[1] == 1 && fat1[2] == 0) { // IF PANS EQUALS TO 1 AND TODOSOSNUMEROS EQUALS TO ZERO(WHICH MEANS THERE IS NO MONOMIO LEFT)
      //  naonao = fat1[0]
        daprafat1 = true
        }
+       */
    
        daprafat2 = false
+       /*
        if (fat2[1] == 1 && fat2[2] == 0) {
       // tobreak = fat2[0]
        daprafat2 = true
        }
-   
+   */
+
        //console.log('__________________')
 
        if (edge.length > 1) {
