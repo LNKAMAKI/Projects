ac31eff10449833331aba65878f3e38637d2e6d1
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2022-08-06 09:51:12 -0300

    a

---
+++ projects/funcao.js
@@ -0,0 +1,197 @@
+tudocerto = true
+function resolverFuncao() {
+    form = document.getElementById('formula').value
+    console.log(form)
+    
+    numero = '1'
+    expression = []
+    ansd = false
+    position = 0
+    juntar = false
+    anstxt = ''
+    error = false
+    for (qualq in form) {
+        if (String(form[qualq]).search('[0-9]') != -1 || form[qualq].search('[\\.\\(\\)\\+\\-\\x\\/r*=Backspace]') != -1) {
+            if (String(form[qualq]).search('[=Backspace]') == -1) {
+            if (ansd == false) {
+            if (expression.length > 0) {
+            if (String(form[qualq]).search('[0-9]') != -1 ||String(form[qualq]) == '.') {
+                if (ansd == true) {
+                    ansd = false
+                }
+               if (String(expression[expression.length - 1]).search('[0-9]') != -1 || String(expression[expression.length - 1]) == '.' || juntar == true) {
+                expression[position] +=String(form[qualq])
+                juntar = false
+               }else{
+                   position++
+                expression[position] =String(form[qualq])
+               }
+            }else{
+                if (String(form[qualq]) == '-' && String(expression[expression.length - 1]).search('[\\x\\*\\+\\(\\/]') != -1 ||String(form[qualq]) == '-' && expression[expression.length - 1] == '-') {
+                    juntar = true
+                }
+                if (expression.length > 0) {
+                position++
+                }
+                expression[position] =String(form[qualq])
+            }
+        }else{
+            if (String(form[qualq]).search('[0-9]') != -1 ||String(form[qualq]) == '.' ||String(form[qualq]) == '(' ||String(form[qualq]) == '-' ||String(form[qualq]) == '√')  {
+           expression.push(String(form[qualq]))
+            }
+           if (form[qualq] == '-') {
+            juntar = true
+           }}
+            }else{
+                if (String(form[qualq]).search('[0-9]') != -1 ||String(form[qualq]) == '.' ||String(form[qualq]) == '(' ||String(form[qualq]) == '√') {
+                    expression[0] =form[qualq]
+                    ansd = false
+                    error = false
+                }else if (error == false &&String(form[qualq]) != ')'){
+               position++
+                expression[position] =form[qualq]
+                ansd = false
+            }else if(error == true){
+                ansd = false
+                error = false
+                if (String(form[qualq]) == '-') {
+                    expression[0] =form[qualq]
+                    juntar = true
+                }}}}}} 
+    degs = []
+    parents = 0
+    for (e = 0; e < expression.length; e++) {
+        if (expression[e] == '(') {
+            continuar = false
+            stop2 = false
+            pares = 0
+            degs.push({})
+            degs[parents].start = e
+            calcular = ''
+            for (n = e + 1; stop2 == false && n < expression.length; n++) {
+                if (expression[n] == '(') {
+                    pares++
+                    continuar = true
+                    degs[parents].par = pares
+                }else if (continuar == true && expression[n] == ')'){ 
+                        if (pares == 1) {
+                            continuar = false
+                        }
+                        pares--
+                }else if (continuar == false && expression[n] == ')') {
+                    if (degs[parents].par == undefined) {
+                        degs[parents].par = 0
+                    }
+                    stop2 = true
+                    degs[parents].end =form[qualq]
+                }}
+            parents++
+        }} 
+    stop = false
+   for (h = 0; h < degs.length && stop == false; h++){
+    if (degs[h].end == undefined) {
+        stop = true
+        tudocerto = false
+    }
+   }
+}
+
+function fazerConta(anterior) {
+    //Primeiro raizes e potências
+    for (n = 0; n < expression.length; n++) {
+    if(expression[n] == '*' && expression[n - 1] != '(' && expression[n - 1] != ')' && expression[n + 1] != '(' && expression[n + 1] != ')' && expression[n + 1] != '√') {
+        expression[n + 1] = String(Number(expression[n - 1]) ** Number(expression[n + 1]))
+        expression.splice(n - 1, 2)
+        n = n - 3 // n = n - 3
+        if (expression[n + 1] == '(' && expression[n + 3] == ')') {
+            expression.splice(n + 3, 1)
+            expression.splice(n + 1, 1)
+            n = n - 3
+        }
+    }else if(expression[n] == '√' && expression[n + 1] != '(' ) {
+        expression[n + 1] = String(Number(expression[n + 1]) ** (1/2))
+        expression.splice(n, 1)
+        n = n - 2
+        if (expression[n + 1] == '(' && expression[n + 3] == ')') {
+            expression.splice(n + 3, 1)
+            expression.splice(n + 1, 1)
+            n = n - 3
+        }}}
+    // Segundo multiplicações e divisões
+    for (n = 0; n < expression.length; n++) {
+    if(expression[n] == 'x' && expression[n - 1] != '(' && expression[n - 1] != ')' && expression[n + 1] != '(' && expression[n + 1] != ')' && expression[n + 1] != '√' && String(expression[n + 2]).search('\\*') == -1 && String(expression[n - 2]).search('\\*') == -1) {
+    expression[n + 1] = String(Number(expression[n - 1]) * Number(expression[n + 1]))
+    expression.splice(n - 1, 2)
+    n = n - 3
+    if (expression[n + 1] == '(' && expression[n + 3] == ')') {
+        expression.splice(n + 3, 1)
+        expression.splice(n + 1, 1)
+        n = n - 3
+    }
+    }else if(expression[n] == '/' && expression[n - 1] != '(' && expression[n - 1] != ')' && expression[n + 1] != '(' && expression[n + 1] != ')' && expression[n + 1] != '√' && String(expression[n + 2]).search('\\*') == -1 && String(expression[n - 2]).search('\\*') == -1) {
+    expression[n + 1] = String(Number(expression[n - 1]) / Number(expression[n + 1]))
+    expression.splice(n - 1, 2)
+    n = n - 3
+    if (expression[n + 1] == '(' && expression[n + 3] == ')') {
+        expression.splice(n + 3, 1)
+        expression.splice(n + 1, 1)
+        n = n - 3
+    }}}
+    if (expression.length > 1) {
+        for (n = 0; n < expression.length && expression.length != 1; n++) {
+            if (expression[n] == '+' && expression[n - 1] != '(' && expression[n - 1] != ')' && expression[n + 1] != '(' && expression[n + 1] != ')' && expression[n + 1] != '√' && String(expression[n + 2]).search('[x\\/\\*]') == -1 && String(expression[n - 2]).search('[x\\/\\*\\-]') == -1) {
+                expression[n + 1] = String(Number(expression[n - 1]) + Number(expression[n + 1]))
+                expression.splice(n - 1, 2)
+                n = n - 3
+                if (expression[n + 1] == '(' && expression[n + 3] == ')') {
+                    expression.splice(n + 3, 1)
+                    expression.splice(n + 1, 1)
+                    n = n - 3
+                }
+            }else if(expression[n] == '-' && expression[n - 1] != '(' && expression[n - 1] != ')' && expression[n + 1] != '(' && expression[n + 1] != ')' && expression[n + 1] != '√' && String(expression[n + 2]).search('[x\\/\\*]') == -1 && String(expression[n - 2]).search('[x\\/\\*\\-]') == -1) {
+                expression[n + 1] = String(Number(expression[n - 1]) - Number(expression[n + 1]))
+                expression.splice(n - 1, 2)
+                n = n - 3
+                if (expression[n + 1] == '(' && expression[n + 3] == ')') {
+                    expression.splice(n + 3, 1)
+                    expression.splice(n + 1, 1)
+                    n = n - 3
+                }
+            }else if(expression[n] == 'x' && expression[n - 1] != '(' && expression[n - 1] != ')' && expression[n + 1] != '(' && expression[n + 1] != ')' && expression[n + 1] != '√' && String(expression[n + 2]).search('\\*') == -1 && String(expression[n - 2]).search('\\*') == -1) {
+                expression[n + 1] = String(Number(expression[n - 1]) * Number(expression[n + 1]))
+                expression.splice(n - 1, 2)
+                n = n - 3
+                if (expression[n + 1] == '(' && expression[n + 3] == ')') {
+                    expression.splice(n + 3, 1)
+                    expression.splice(n + 1, 1)
+                    n = n - 3
+                }
+            }else if(expression[n] == '/' && expression[n - 1] != '(' && expression[n - 1] != ')' && expression[n + 1] != '(' && expression[n + 1] != ')' && expression[n + 1] != '√' && String(expression[n + 2]).search('\\*') == -1 && String(expression[n - 2]).search('\\*') == -1) {
+                expression[n + 1] = String(Number(expression[n - 1]) / Number(expression[n + 1]))
+                expression.splice(n - 1, 2)
+                n = n - 3
+                if (expression[n + 1] == '(' && expression[n + 3] == ')') {
+                    expression.splice(n + 3, 1)
+                    expression.splice(n + 1, 1)
+                    n = n - 3
+                }
+            }else if(expression[n] == '*' && expression[n - 1] != '(' && expression[n - 1] != ')' && expression[n + 1] != '(' && expression[n + 1] != ')' && expression[n + 1] != '√') {
+                expression[n + 1] = String(Number(expression[n - 1]) ** Number(expression[n + 1]))
+                expression.splice(n - 1, 2)
+                n = n - 3
+                if (expression[n + 1] == '(' && expression[n + 3] == ')') {
+                    expression.splice(n + 3, 1)
+                    expression.splice(n + 1, 1)
+                    n = n - 3
+                }
+            }else if(expression[n] == '√' && expression[n + 1] != '(') {
+                expression[n + 1] = String(Number(expression[n + 1]) ** (1/2))
+                expression.splice(n, 1)
+                n = n - 2
+                if (expression[n + 1] == '(' && expression[n + 3] == ')') {
+                    expression.splice(n + 3, 1)
+                    expression.splice(n + 1, 1)
+                    n = n - 3
+                }}}}
+                return expression
+    }
