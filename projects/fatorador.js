a9199e5f7a440bd32106cad73b83a22848107646
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2022-08-12 10:47:23 -0300

    Update fatorador.js

--- projects/fatorador.js
+++ projects/fatorador.js
@@ -36,7 +36,7 @@ function ir() {
         expression.push(ground);
       } }
 
-    resultadogeral = FATORAR(expression,true);
+    resultadogeral = FATORAR2(expression);
     resultado = resultadogeral[0];
     devtools(false)
     document.getElementById("result").innerHTML = "Resultado:";
@@ -3027,6 +3027,7 @@ function FATORE(q) {
     }}
 
   function FATORAR2(expression_2,refat) {
+    alt = 2
     startexp_2 = [...expression_2];
 
   if (expression_2[0] !== "-" && expression_2[0] !== "+") {
@@ -3564,7 +3565,7 @@ function FATORE(q) {
    
    roller_2 = [];
 
-   /*
+   
   for (eep in miss_2) {
     for (quad in miss_2) {
       if (quad != eep) {
@@ -3573,15 +3574,12 @@ function FATORE(q) {
         for (vespa in miss_2[eep].aparicoes) {
           if (miss_2[quad].aparicoes.indexOf(miss_2[eep].aparicoes[vespa]) != -1) {
             repeated.push(miss_2[eep].aparicoes[vespa]);
-
             repwri += "," + miss_2[eep].aparicoes[vespa];
-
             if (
               roller_2.find(function (roller_2) {
                 return roller_2.rept == repwri;
               }) == undefined
             ) {
-              //console.log('PODE PUSHAR',{rept: repwri,opl:repeated,position: [eep,quad]}, repeated, repeated.length)
               roller_2.push({
                 rept: repwri,
                 opl: [...repeated],
@@ -3614,68 +3612,44 @@ function FATORE(q) {
                     return roller_2.rept == repwri;
                   })
                   .position.push(quad);
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-  */
-
+              }} }}}}}
+    
+              /*
   for (eep in miss_2) {
     monspos_ = []
     for (b in miss_2[eep].aparicoes) {
       monpos = scarecrow_2[miss_2[eep].aparicoes[b]].positions[scarecrow_2[miss_2[eep].aparicoes[b]].divididos.indexOf(miss_2[eep].dividido)]
       monspos_.push(monpos)
     }
- 
 for (quad in miss_2) {
-
 if (quad != eep) {
-  
   repeated = [];
   repwri = "";
-
   monspos2 = []
     for (b in miss_2[quad].aparicoes) {
-      
       monpos = scarecrow_2[miss_2[quad].aparicoes[b]].positions[scarecrow_2[miss_2[quad].aparicoes[b]].divididos.indexOf(miss_2[quad].dividido)]
       monspos2.push(monpos)
     }
-    
     rap = ''
-
     monspos = []
     result = []
   for (vespa in miss_2[eep].aparicoes) {
-    
     cango = true
     monpos = scarecrow_2[miss_2[eep].aparicoes[vespa]].positions[scarecrow_2[miss_2[eep].aparicoes[vespa]].divididos.indexOf(miss_2[eep].dividido)]
-
     if (miss_2[quad].aparicoes.indexOf(miss_2[eep].aparicoes[vespa]) != -1) {
-
       if (monspos.indexOf(monpos) == -1) {
         monspos.push(monpos)
-        
           }else{
             cango = false
           }
-
       posmon = monspos2[miss_2[quad].aparicoes.indexOf(miss_2[eep].aparicoes[vespa])]
-    
       repeated.push(miss_2[eep].aparicoes[vespa]);
-
       rap+= ',' + miss_2[eep].aparicoes[vespa]
 
       if (roller_2.find(function(roller_2) {return roller_2.rept == rap}) != undefined) {
-
-        
         if (cango == true && monspos.indexOf(posmon) == -1) {
-
         monspos.push(posmon)
         what = roller_2.find(function(roller_2) {return roller_2.rept == rap})
-      
       for (h in monspos) {
         if (what.mons.indexOf(monspos[h]) == -1) {
           what.mons.push(monspos[h])
@@ -3683,7 +3657,6 @@ if (quad != eep) {
     }else{
       monspos.splice(monspos.length - 1,1)
     }
-    
   }else{
       if (monspos.indexOf(posmon) == -1 && cango == true) {
         monspos.push(posmon)
@@ -3711,13 +3684,11 @@ if (quad != eep) {
       rap = nov
     } }
       repwri = rap          
-
       if (
         roller_2.find(function (roller_2) {
           return roller_2.rept == repwri;
         }) == undefined
       ) {
-        
         roller_2.push({
           mons: [...result],
           rept: repwri,
@@ -3743,7 +3714,7 @@ if (quad != eep) {
             })
             .position.push(quad);
         }}}}}}}
-
+*/
    for (r in roller_2) {
        roll_2.push({repetidos: roller_2[r].opl, posições: roller_2[r].position, way: roller_2[r].rept})
    }
@@ -3937,126 +3908,52 @@ if (quad != eep) {
          roller_2 = [];
   
          for (eep in miss_2) {
-          monspos_ = []
-          for (b in miss_2[eep].aparicoes) {
-            monpos = scarecrow_2[miss_2[eep].aparicoes[b]].positions[scarecrow_2[miss_2[eep].aparicoes[b]].divididos.indexOf(miss_2[eep].dividido)]
-            monspos_.push(monpos)
-          }
-       
-    for (quad in miss_2) {
-      if (quad != eep) {
+            for (quad in miss_2) {
+              if (quad != eep) {
+                repeated = [];
+                repwri = "";
+                for (vespa in miss_2[eep].aparicoes) {
+                  if (miss_2[quad].aparicoes.indexOf(miss_2[eep].aparicoes[vespa]) != -1) {
+                    repeated.push(miss_2[eep].aparicoes[vespa]);
+                    repwri += "," + miss_2[eep].aparicoes[vespa];
+                    if (
+                      roller_2.find(function (roller_2) {
+                        return roller_2.rept == repwri;
+                      }) == undefined
+                    ) {
+                      roller_2.push({
+                        rept: repwri,
+                        opl: [...repeated],
+                        position: [eep, quad],
+                      });
+                    } else {
+                      if (
+                        roller_2
+                          .find(function (roller_2) {
+                            return roller_2.rept == repwri;
+                          })
+                          .position.indexOf(eep) == -1
+                      ) {
+                        roller_2
+                          .find(function (roller_2) {
+                            return roller_2.rept == repwri;
+                          })
+                          .position.push(eep);
+                      }
         
-        repeated = [];
-        repwri = "";
-        monspos2 = []
-          for (b in miss_2[quad].aparicoes) {
-            
-            monpos = scarecrow_2[miss_2[quad].aparicoes[b]].positions[scarecrow_2[miss_2[quad].aparicoes[b]].divididos.indexOf(miss_2[quad].dividido)]
-            monspos2.push(monpos)
-          }
-          
-          rap = ''
-
-          monspos = []
-          result = []
-        for (vespa in miss_2[eep].aparicoes) {
-          
-          cango = true
-          monpos = scarecrow_2[miss_2[eep].aparicoes[vespa]].positions[scarecrow_2[miss_2[eep].aparicoes[vespa]].divididos.indexOf(miss_2[eep].dividido)]
-
-          if (miss_2[quad].aparicoes.indexOf(miss_2[eep].aparicoes[vespa]) != -1) {
-
-            if (monspos.indexOf(monpos) == -1) {
-              monspos.push(monpos)
-              
-                }else{
-                  cango = false
-                }
-
-            posmon = monspos2[miss_2[quad].aparicoes.indexOf(miss_2[eep].aparicoes[vespa])]
-          
-            repeated.push(miss_2[eep].aparicoes[vespa]);
-
-            rap+= ',' + miss_2[eep].aparicoes[vespa]
-
-            if (roller_2.find(function(roller_2) {return roller_2.rept == rap}) != undefined) {
-              if (cango == true && monspos.indexOf(posmon) == -1) {
-              monspos.push(posmon)
-              what = roller_2.find(function(roller_2) {return roller_2.rept == rap})
-            
-            for (h in monspos) {
-              if (what.mons.indexOf(monspos[h]) == -1) {
-                
-                what.mons.push(monspos[h])
-              }}
-          }else{
-            monspos.splice(monspos.length - 1,1)
-          }
-          
-        }else{
-            if (monspos.indexOf(posmon) == -1 && cango == true) {
-              monspos.push(posmon)
-              result.push(monpos)
-              result.push(posmon)
-          }else{
-
-            if (cango == true) {
-              monspos.splice(monspos.length - 1,1)
-              }else{
-              cango = true
-              }
-
-            parar = false
-
-            novorap = ''
-            for (h = rap.length - 1; h >= 0; h--) {
-              
-              if (rap[h] == ',' && parar == false) {
-                parar = true
-              }else if(parar == true) {
-                novorap+= rap[h]
-            }
-          }
-
-          nov = ''
-          for (u = novorap.length - 1; u >= 0; u--) {
-            nov+= novorap[u]
-          }
-            rap = nov
-          }}
-            repwri = rap          
-
-            if (
-              roller_2.find(function (roller_2) {
-                return roller_2.rept == repwri;
-              }) == undefined
-            ) {
-              
-              roller_2.push({
-                mons: [...result],
-                rept: repwri,
-                opl: [...repeated],
-                position: [eep, quad]
-              });
-             
-            } else {
-              if (roller_2.find(function (roller_2) {return roller_2.rept == repwri;}).position.indexOf(eep) == -1) {
-                roller_2.find(function (roller_2) {return roller_2.rept == repwri;})
-                  .position.push(eep);
-              }
-              if (
-                roller_2
-                  .find(function (roller_2) {
-                    return roller_2.rept == repwri;
-                  })
-                  .position.indexOf(quad) == -1
-              ) {
-                roller_2
-                  .find(function (roller_2) {
-                    return roller_2.rept == repwri;
-                  })
-                  .position.push(quad);
-              }} }}} } }
+                      if (
+                        roller_2
+                          .find(function (roller_2) {
+                            return roller_2.rept == repwri;
+                          })
+                          .position.indexOf(quad) == -1
+                      ) {
+                        roller_2
+                          .find(function (roller_2) {
+                            return roller_2.rept == repwri;
+                          })
+                          .position.push(quad);
+                      }} }}}}}
 
   for (r in roller_2) {
     roll_2.push({repetidos: roller_2[r].opl, posições: roller_2[r].position, way: roller_2[r].rept})
@@ -4723,6 +4620,11 @@ if (quad != eep) {
    
    if (numsdesfat.length > 0) {
     console.warn('VAI TER QUE FATORAR DE NOVO')
+    console.log('youdumb')
+    for (j in youdumb_2) {
+        console.log(youdumb_2[j].w.what)
+    }
+    console.log(pans_2)
     todososnumeros_2 = []
     for (acaba in monomios_2) {
         todososnumeros_2.push(acaba)
