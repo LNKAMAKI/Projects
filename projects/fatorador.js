f5b7ec486e84e8e650e372f05b6904e9c8d8078d
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2022-08-18 11:28:51 -0300

    Update fatorador.js

--- projects/fatorador.js
+++ projects/fatorador.js
@@ -359,34 +359,9 @@ function ir() {
         comofica.push(ground);
       }}
     monomios[bye].numero = comofica;
-  }
-  }
+  }}
   // AQUI ACABA O CÓDIGO PARA SOMAR OS MONÔMIOS
-     /*
-     function versetem(comp) {
-      podeir = true;
-      qual = -1;
-      for (meow in segs) {
-        array1 = [];
-        for (d in segs[meow].car) {
-          array1.push(segs[meow].car[d]);
-        }
-        array2 = [];
-        for (d in comp) {
-          array2.push(comp[d]);
-        }
-        if (segs[meow].car.length < comp.length) {
-          divisao = DIVIDIR(VAI(segs[meow].car), VAI(comp));
-        } else {
-          divisao = DIVIDIR(VAI(comp), VAI(segs[meow].car));
-        }
-        if (divisao == "" || divisao == "-") {
-          podeir = false;
-          qual = meow;
-        }}
-      return podeir;
-    }
-     */
+
      ////console.log('--------------------------------------------MOnOMIOS--------------------------------------')
      reps = []
      segs = []
