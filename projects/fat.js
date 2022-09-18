4cf4ef5541da687307912312cd899b2210cb42f9
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2022-09-18 12:57:25 -0300

    Update fat.js

--- projects/fat.js
+++ projects/fat.js
@@ -36,12 +36,13 @@ loaded = false
         
         get1('resol').innerHTML = ''
 
-get1('resol').innerHTML+= ' <div class="center" id="nopad"> <img src="/projects/imagens/rar.png" alt="" class="ar"><span>Resolução</span> </div> <div class="cont"><div class="center" classname="top"><img src="/projects/imagens/rar.png" alt="" class="ar"><span>Engine</span></div><div class="cont"><div class="center" classname="top"><img src="/projects/imagens/rar.png" alt="" class="ar"><span>Segs</span></div></div><div class="cont"><div class="center"  classname="top"><img src="/projects/imagens/rar.png" alt="" class="ar"><span>Miss</span></div></div><div class="cont"><div class="center"  classname="top"><img src="/projects/imagens/rar.png" alt="" class="ar"><span>Roll</span></div></div><div class="cont"><div class="center"  classname="top"><img src="/projects/imagens/rar.png" alt="" class="ar"><span>Youdumb</span></div></div></div><div class="cont"> <div class="center"  classname="top"> <img src="/projects/imagens/rar.png" alt="" class="ar"> <span> Passo a passo</span></div><p></p></div>'
+get1('resol').innerHTML+= ' <div class="center" id="nopad"> <img src="/projects/imagens/rar.png" alt="" class="ar"><span>Resolução</span> </div> <div class="cont" name="ti"><div class="center" classname="top"><img src="/projects/imagens/rar.png" alt="" class="ar"><span>Engine</span></div><div class="cont" name="ti"><div class="center" classname="top"><img src="/projects/imagens/rar.png" alt="" class="ar"><span>Segs</span></div></div><div class="cont" name="ti"><div class="center"  classname="top"><img src="/projects/imagens/rar.png" alt="" class="ar"><span>Miss</span></div></div><div class="cont" name="ti"><div class="center"  classname="top"><img src="/projects/imagens/rar.png" alt="" class="ar"><span>Roll</span></div></div><div class="cont" name="ti"><div class="center"  classname="top"><img src="/projects/imagens/rar.png" alt="" class="ar"><span>Youdumb</span></div></div></div><div class="cont" name="ti"> <div class="center"  classname="top"> <img src="/projects/imagens/rar.png" alt="" class="ar"> <span> Passo a passo</span></div><p></p></div>'
         conts = get2('cont')
         cents = get2('center')
         ars = get2('ar')
         cs = []
-        cn = conts[2]
+        ts = document.getElementsByName('ti')
+        cn = ts[1]
 
         cn.innerHTML+= '<div class="dev"></div>'
         for (h in res[5]) {
