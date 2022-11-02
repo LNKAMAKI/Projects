815296c09a859d4b6e3fb94346ab7a85fdcaf57d
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2022-11-02 20:37:06 -0300

    Update fat.js

--- projects/fat.js
+++ projects/fat.js
@@ -1690,7 +1690,7 @@ function doTheFactoring(exal) {
                   for (vespa in miss_2[eep].aparicoes) { // *miss_2[eep].aparicoes = [1,2], *miss_2[eep].aparicoes[0] = 1
                     if (compar.indexOf(varib[vespa]) == -1) { // *compar.indexOf(varib[0]/1)
                     compar.push(varib[vespa]) // *varib[0] = 1
-                    if (miss_2[quad].aparicoes.indexOf(miss_2[eep].aparicoes[vespa]) != -1 && compar.indexOf(varib2[miss_2[quad].aparicoes.indexOf(miss_2[eep].aparicoes[vespa])]) == -1) { // *[1,2].indexOf(1) && compar.indexOf(varib_2[0]/2) == -1
+                    if (miss_2[quad].aparicoes.indexOf(miss_2[eep].aparicoes[vespa]) != -1 && compar.indexOf(varib2[miss_2[quad].aparicoes.indexOf(miss_2[eep].aparicoes[vespa])]) == -1) { // *[1,2].indexOf(1) != -1 && compar.indexOf(varib_2[0]/2) == -1
                       compar.push(varib2[miss_2[quad].aparicoes.indexOf(miss_2[eep].aparicoes[vespa])]) // compar.push(varib2[0]/2)
                         
                         repeated.push(miss_2[eep].aparicoes[vespa]); // *repeated.push(miss_2[eep].aparicoes[0]) (1)
