1026b288d6daac699e2d836fee79c4184964ea06
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2022-11-02 09:46:53 -0300

    Update words-sort.js

--- projects/words-sort.js
+++ projects/words-sort.js
@@ -10,56 +10,25 @@ console.log(sortWords(listaPalavras,'word'))
 
 function sortWords(palavrasPrimitivas,s) {
 for (palavra in palavrasPrimitivas) {
-    //⬜
     word = palavrasPrimitivas[palavra][s]
     wordToNumber = ''
     for (letra = 0; letra < word.length; letra++) {
-        //⬜
-        //⬜
-
         if (letra != word.length - 1) {
-            //⬜
             wordToNumber += `${alfabeto.indexOf(word[letra])}.`
         } else {
             wordToNumber += `${alfabeto.indexOf(word[letra])}`
-        }
-    }
-    //⬜
-
-    //⬜
-
+        }}
     palavrasNumericas.push(wordToNumber)
     posicaoPalavras.push(palavra)
-
-    //⬜
-    //⬜
 }
-
-//⬜
-
-
 for (pNumber in palavrasNumericas) {
-    //⬜
-    //⬜
-    //⬜
-    //⬜
-
     stop = false
     for (n = 0; stop == false && n < pNumber; n++) {
-        //⬜
-
         if (n != pNumber) {
-            //⬜
-            //⬜
             palavraAnalisar = []
-           
-            //⬜
-            
             numint = ''
             for (num = 0; num < palavrasNumericas[n].length; num++) {
-                   //⬜
                    if (palavrasNumericas[n][num] == '.') {
-                    //⬜
                     palavraAnalisar.push(numint)
                     numint = ''
                    }else if(num == palavrasNumericas[n].length - 1){
@@ -67,21 +36,11 @@ for (pNumber in palavrasNumericas) {
                         palavraAnalisar.push(numint)
                    }else{
                     numint += `${palavrasNumericas[n][num]}`
-                   }
-            }
-            //⬜
-
-            //⬜
-            //⬜
+                   }  }
             palavraOrigin = []
-
-            //⬜
-            
             numint = ''
             for (num = 0; num < palavrasNumericas[pNumber].length; num++) {
-                   //⬜
                    if (palavrasNumericas[pNumber][num] == '.') {
-                    //⬜
                     palavraOrigin.push(numint)
                     numint = ''
                    }else if(num == palavrasNumericas[pNumber].length - 1){
@@ -89,102 +48,48 @@ for (pNumber in palavrasNumericas) {
                         palavraOrigin.push(numint)
                    }else{
                     numint += `${palavrasNumericas[pNumber][num]}`
-                   }
-            }
-            //⬜
-
-            //⬜
-
-            //⬜
+                   } }
            if (palavraOrigin.length > palavraAnalisar.length) {
-             //⬜
              pare = false
              for (numb = 0; numb < palavraAnalisar.length && pare == false; numb++) {
-              //⬜
-              //⬜
               if (Number(palavraAnalisar[numb]) < Number(palavraOrigin[numb])) {
-                //⬜
                 pare = true
-                //⬜
               }else if (Number(palavraAnalisar[numb]) > Number(palavraOrigin[numb])) {
-                //⬜
                 pare = true
-                //⬜
-                //⬜
-                //⬜
                 stop = true
-
-                //⬜
-              
-                //⬜
-                //⬜
-                //⬜
                 palavraSubir = palavrasNumericas[pNumber]
                 wordposition = posicaoPalavras[pNumber]
                 for (posicao = pNumber; posicao > n; posicao--) {
-                  //⬜
                   palavrasNumericas[posicao] = palavrasNumericas[posicao - 1]
                   posicaoPalavras[posicao] = posicaoPalavras[posicao - 1]
                 }
                 palavrasNumericas[n] = palavraSubir
                 posicaoPalavras[n] = wordposition
-                //⬜
-                //⬜
               }else{
                   pare = false
-              }
-             }
+              }}
            }else{
-            //⬜
             pare = false
             for (numb = 0; numb < palavraOrigin.length && pare == false; numb++) {
-                //⬜
-                //⬜
                 if (Number(palavraAnalisar[numb]) < Number(palavraOrigin[numb])) {
-                    //⬜
                     pare = true
-                    //⬜
                   }else if (Number(palavraAnalisar[numb]) > Number(palavraOrigin[numb])) {
-                    //⬜
                     pare = true
-                    //⬜
-                    //⬜
-                    //⬜
                     stop = true
-
-                    //⬜
-
-                    //⬜
-                    //⬜
-                    //⬜
                     palavraSubir = palavrasNumericas[pNumber]
                     wordposition = posicaoPalavras[pNumber]
                     for (posicao = pNumber; posicao > n; posicao--) {
-                      //⬜
                       palavrasNumericas[posicao] = palavrasNumericas[posicao - 1]
                       posicaoPalavras[posicao] = posicaoPalavras[posicao - 1]
                     }
                     palavrasNumericas[n] = palavraSubir
                     posicaoPalavras[n] = wordposition
-                    //⬜
-                    //⬜
                   }else{
                       pare = false
-                  }
-               }
-           }
-          
-        }
-    }
-
-}
-//⬜
-
+                  }}}}}}
 newlista = []
 for (p in palavrasNumericas) {
-//⬜
 newlista.push(palavrasPrimitivas[posicaoPalavras[p]])
 }
-
 return newlista
 }
