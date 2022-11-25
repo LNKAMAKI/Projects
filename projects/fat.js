5126193933ac65f699f769f1d5dec9a828c19e7c
Ellen Kaori <akemitakihi@gmail.com>
2022-11-25 20:03:02 -0300

    Update fat.js

--- projects/fat.js
+++ projects/fat.js
@@ -3728,7 +3728,6 @@ fat2 = new CreateEngine2().FATORAR(ep,false)
               return fires
             }
 
-    
             function sortWords(palavrasPrimitivas) {
                 alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
                 palavrasNumericas = []
