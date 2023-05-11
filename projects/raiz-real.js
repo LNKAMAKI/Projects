9d6df0bd3b161a946658429d740db22d9281dc13
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2023-05-11 09:17:59 -0300

    alright

---
+++ projects/raiz-real.js
@@ -0,0 +1,167 @@
+function showresult() {
+    console.log('yaya')
+    expression = document.getElementById('write').value
+    a = ''
+    b = ''
+    c = ''
+    ai = ''
+    bi = ''
+    ci = ''
+    for (char in expression) {
+        sim = expression[char]
+        console.log(sim)
+    }
+}
+
+function changeexp (pexpind) {
+    pexps = document.getElementsByClassName('pexp')
+ //console.log('HEY',pexps[pexpind].innerText)
+ get1('write').value = pexps[pexpind].innerText
+}
+
+function load() {
+    pexps = document.getElementsByClassName('pexp')
+for (sure = 0; sure < pexps.length; sure++) {
+    //console.log(pexps[sure])
+    pexps[sure].setAttribute('onclick',`changeexp(${sure})`)
+}
+
+  // initialize content scroll
+    conts = get2('cont')
+    cents = get2('center')
+    ars = get2('ar')
+    mecams = [] 
+    late = -1   
+    sai = 0
+    for (bob = 0; bob < cents.length; bob++) {
+    c = new CreateFunc(bob,'close')
+        c.SetDefault()
+        c.AddEvent()
+    }
+    //
+    
+    }
+    
+    function get1(thist) {
+        return document.getElementById(thist)
+    }
+    
+    function get2(thist) {
+        return document.getElementsByClassName(thist)
+    }
+    // code for content scroll engine
+    function CreateFunc(ind,action,number){
+        this.ind = ind
+        this.par1 = document.getElementsByClassName('ar')[this.ind]
+        this.par2 = get2('cont')[this.ind]
+        this.par3 = get2('center')[this.ind]
+        this.l = late
+        if (action != "open") {
+        this.clicked = false
+        this.par1.style.transform = 'rotate(0deg)'
+        }else{
+        this.clicked = true
+        this.par1.style.transform = 'rotate(90deg)'
+        }
+    
+        late++
+        this.a = `j${late}`
+        this.number = number
+        h1 = 0
+        h2 = 100
+        heightInPx = String(this.par3.offsetHeight) + 'px'
+       
+        this.SetDefault= function () {
+            heightInPx = String(this.par3.offsetHeight) + 'px'
+           if (this.par3.innerText != 'Relações' && this.par3.innerText != 'Repetições' && this.par3.innerText != 'Combinações' && this.par3.innerText != 'Agrupamentos' ) {
+            this.par2.style.height = heightInPx
+            this.clicked = false
+           }else{
+                this.par2.style.height = '28px'
+                this.clicked = false
+           } }
+        this.par1.setAttribute
+        this.AddEvent = function() {
+           this.par1.setAttribute('onclick',`cli(${this.clicked},${this.ind},'${this.a}',${this.number},${this.par3.offsetHeight})`)
+        }}
+    
+        function cli(state,index,jin,n,jor) {
+            me = document.getElementsByClassName('ar')
+            this.jin = jin
+            this.par1 = me[index]
+            this.par2 = conts[index]
+            this.par3 = cents[index]
+            this.n = n
+            beg = this.par2.offsetHeight
+            this.par3.style.height = jor + 'px'
+            this.par2.style.height = 'fit-content'
+            sul = this.par2.offsetHeight
+            search = mecams.find(function(mecams){
+                return mecams.created == this.jin
+               })
+        
+            if (search != undefined) {
+                search.comp++
+               }else{
+                   if (state == true) {
+                    mecams.push({created:this.jin,comp:1,st:jor,end:sul,cl:state})
+                   }else{
+                    mecams.push({created:this.jin,comp:1,st:beg,end:sul,cl:state})
+                   } }
+               search = mecams.find(function(mecams){
+                return mecams.created == this.jin
+               })
+        
+               if (search.cl == false) {
+                txt = String(this.par3.innerText)
+                   busca = txt.search(new RegExp('((Agrupamentos)|(Relações)|(Combinações)|(Repetições))','g'))
+        
+                if (this.par3.innerText == 'Resolução') {
+                    ih = search.st - 10
+                    eh = sul - 10
+                   }else{
+                    ih = search.st
+                    eh = sul
+                   }
+        
+            css = `@keyframes ${jin}${search.comp} {
+                 0% {
+                    height: ${ih}px;
+                }
+                100% {
+                    height: ${eh}px;
+                }}`
+                this.par2.style.animation = `${jin}${search.comp} 0.2s`
+                this.par2.style.height = 'fit-content'
+        
+               style = document.createElement('style')
+               document.head.appendChild(style)
+               style.appendChild(document.createTextNode(css))
+               this.par1.style.transform = 'rotate(90deg)'
+               search.cl = true
+        }else{
+            if (this.par3.innerText == 'Resolução') {
+                ih = search.st - 10
+                eh = sul - 10
+               }else{
+                ih = search.st
+                eh = sul
+               }
+        
+            css = `@keyframes ${jin}${search.comp} {
+                0% {
+                   height: ${eh}px;
+               }
+               100% {
+                   height: ${ih}px;
+               }}`
+            this.par2.style.animation = `${jin}${search.comp} 0.2s`
+            this.par2.style.height = `${ih}px`
+           style = document.createElement('style')
+           document.head.appendChild(style)
+           style.appendChild(document.createTextNode(css))
+           this.par1.style.transform = 'rotate(0deg)'
+           search.cl = false
+        }}
+    //
+        
