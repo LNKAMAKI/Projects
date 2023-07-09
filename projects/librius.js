b488c86672b63ce2ee006a5e4f3073e363a661e4
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2023-07-08 21:23:10 -0300

    Update librius.js

--- projects/librius.js
+++ projects/librius.js
@@ -1,31 +1,4 @@
-/*
-function vocab() {
-    section = document.getElementsByTagName('section')[0]
-    section.innerHTML = ''
-    frame = document.createElement('iframe')
-    frame.setAttribute('src', '/librius-materials/vocab.html')
-    section.appendChild(frame)
-    frame.setAttribute('scrolling','no')
-    
-    iframe = frame
-console.log(iframe)
-
-if (iframe != null) {
-function resizeIframe() {
-    iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
-  }
-
-  setInterval(function() {
-    var currentHeight = iframe.style.height;
-
-    // check if the iframe content has changed
-    if (currentHeight !== iframe.contentWindow.document.body.scrollHeight + 'px') {
-      resizeIframe();
-    }
-  }, 20)
-}
-    }*/
-
+page = 0
 initialcontent = ''
 function get1(thist) {
     return document.getElementById(thist)
