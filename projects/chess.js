1aca17c24820acabffedb2ed8d3fcb90a2215a0c
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2023-07-14 13:04:23 -0300

    drop-box hierarchy engine made

--- projects/chess.js
+++ projects/chess.js
@@ -1,12 +1,46 @@
+console.log('EYES ON THE HIGHWAY')
+elements = [
+    {html: 'a', objects: [
+        {html: 'a', objects: []}
+    ]},
+    {html: 'a', objects: [
+        {html: 'a', objects: [
+            {html: 'a', objects: []},
+            {html: 'a', objects: []}
+        ]}
+    ]},
+    {html: 'a', objects: [
+        {html: 'a', objects: []}
+    ]}
+]
+console.log('el',elements)
 
-pattern = 1
-for (i = 0; i < 8;i++) {
-    if (pattern == 2) {
-        pattern = 1
-        console.log('⬛⬜⬛⬜⬛⬜⬛⬜')
-    }else{
-        pattern = 2
-        console.log('⬜⬛⬜⬛⬜⬛⬜⬛')
+for (ciyu in elements) {
+ elements[ciyu].id = `${ciyu}`
+ console.log(elements[ciyu])
+}
+loop(elements)
+lists = []
+function loop(arraylist, d) {
+    repeat = false
+    newlist = []
+    console.log(arraylist)
+    for (dial in arraylist) {
+        console.log('')
+        console.log(arraylist[dial])
+        console.log('--------')
+        for (tones in arraylist[dial].objects) {
+            console.log(arraylist[dial].id + `.${tones}`)
+            arraylist[dial].objects[tones].id = arraylist[dial].id + `.${tones}`
+            console.log(dial,arraylist[dial].objects[tones], tones)
+            newlist.push(arraylist[dial].objects[tones])
+        }
+        if (arraylist[dial].objects.length > 0) {
+            repeat = true
+        }
+        console.log('--------')
     }
-    
+    console.log('repeat', newlist)
+    if (repeat == true)
+        loop(newlist)
 }
