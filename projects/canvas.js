7191d97a2f6b8bb77b8351e00894b117573acbd5
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2024-01-03 22:29:04 -0300

    Update canvas.js

--- projects/canvas.js
+++ projects/canvas.js
@@ -23,7 +23,7 @@ mousey = ''
 function animate() {
    // for (t = 0; t < 2;t++) {
     if (pokebolas.length == 0) {
-for (v = 0; v < 3; v++) {
+for (v = 0; v < 7; v++) {
 width = Math.random()*8 + 10
 width = 13
 velx = Number((Math.random()*4).toFixed(0)) + 1
@@ -1342,7 +1342,39 @@ function collision() {
                     pokebolas[rb].x = realposibsx[posibin].x2
                     pokebolas[rb].y = realposibsx[posibin].y2
                     console.log('precisa refazer as colisões anteriores')
-                    // NOT FINISHED
+                    
+                    for (ba = 0; ba < posibin; ba++) {
+                        console.log('precisa refazer essa colisão aqui:')
+                        console.log(realposibsx[ba])
+                        if (realposibsx[ba].colide == true) {
+                        re = detectCollision(realposibsx[ba].a,realposibsx[ba].b,pokebolas[realposibsx[ba].a].fstx,pokebolas[realposibsx[ba].a].fsty,pokebolas[realposibsx[ba].b].x,pokebolas[realposibsx[ba].b].y,true,false)
+                        
+                        if (re != undefined) {
+                        pokebolas[realposibsx[ba].a].x = re.x1
+                        pokebolas[realposibsx[ba].a].y = re.y1
+                        }
+                        }else{
+                            console.log('epa, n se colidem, porém pode ter coisa aí')
+                            re = detectCollision(realposibsx[ba].a,realposibsx[ba].b,pokebolas[realposibsx[ba].a].fstx,pokebolas[realposibsx[ba].a].fsty,pokebolas[realposibsx[ba].b].x,pokebolas[realposibsx[ba].b].y,true,false)
+                            console.log(re)
+                            if (re != undefined) {
+                                if (pokebolas[realposibsx[ba].a].x == pokebolas[realposibsx[ba].a].fstx) {
+                                if (pokebolas[realposibsx[ba].a].velx > 0 && realposibsx[ba].x1 > re.x1 || pokebolas[realposibsx[ba].a].velx < 0 && realposibsx[ba].x1 < re.x1) {
+                                    pokebolas[realposibsx[ba].a].x = re.x1
+                                    pokebolas[realposibsx[ba].a].y = re.y1
+                                    loop = false
+                                }
+                            }else{
+                                if (pokebolas[realposibsx[ba].a].velx > 0 && pokebolas[realposibsx[ba].a].x > re.x1 || pokebolas[realposibsx[ba].a].velx < 0 && pokebolas[realposibsx[ba].a].x < re.x1) {
+                                    pokebolas[realposibsx[ba].a].x = re.x1
+                                    pokebolas[realposibsx[ba].a].y = re.y1
+                                    loop = false
+                                }
+                            }
+                                // WORK MORE ON THIS
+                            }
+                        }
+                    } 
                 }else{
                     console.log('contiuar como estava')
                 }
@@ -1366,7 +1398,6 @@ function collision() {
                     loop = false
                 }
             }
-                // WORK MORE ON THIS
             }
         }
         }
