784582539cd25e979b3b996265dd7610a7e4e183
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2024-01-03 22:20:31 -0300

    implementing missing conditions

--- projects/canvinha2.js
+++ projects/canvinha2.js
@@ -8,6 +8,25 @@ for (ba = 0; ba < posibin; ba++) {
     pokebolas[realposibsx[ba].a].x = re.x1
     pokebolas[realposibsx[ba].a].y = re.y1
     }
+    }else{
+        console.log('epa, n se colidem, porém pode ter coisa aí')
+        re = detectCollision(realposibsx[ba].a,realposibsx[ba].b,pokebolas[realposibsx[ba].a].fstx,pokebolas[realposibsx[ba].a].fsty,pokebolas[realposibsx[ba].b].x,pokebolas[realposibsx[ba].b].y,true,false)
+        console.log(re)
+        if (re != undefined) {
+            if (pokebolas[realposibsx[ba].a].x == pokebolas[realposibsx[ba].a].fstx) {
+            if (pokebolas[realposibsx[ba].a].velx > 0 && realposibsx[ba].x1 > re.x1 || pokebolas[realposibsx[ba].a].velx < 0 && realposibsx[ba].x1 < re.x1) {
+                pokebolas[realposibsx[ba].a].x = re.x1
+                pokebolas[realposibsx[ba].a].y = re.y1
+                loop = false
+            }
+        }else{
+            if (pokebolas[realposibsx[ba].a].velx > 0 && pokebolas[realposibsx[ba].a].x > re.x1 || pokebolas[realposibsx[ba].a].velx < 0 && pokebolas[realposibsx[ba].a].x < re.x1) {
+                pokebolas[realposibsx[ba].a].x = re.x1
+                pokebolas[realposibsx[ba].a].y = re.y1
+                loop = false
+            }
+        }
+            // WORK MORE ON THIS
+        }
     }
-    // DON'T FORGET TO IMPLEMENT THE CODE HERE TOO
-}
+} 
