1dd5383fcbcc211612fbc140c248b0d75431bc00
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2024-07-27 16:05:09 -0300

    Update waves.js

--- projects/waves.js
+++ projects/waves.js
@@ -8,12 +8,11 @@ canv = document.getElementById('canvas')
 c = canv.getContext('2d')
 console.log(x)
 
-
 function animate() {
     x = 0
     c.clearRect(0,0,300,150)
     // function = A*sen(2*Math.PI/comp*(x + wt))
-    for (i = 0; i < 37;i++) {
+    for (i = 0; i < 75;i++) {
         if (timer - x*0.1 >= 0) {
             y = starty + 30*Math.sin(0 - 0.4*(timer - x*0.1))
         }else{
@@ -26,11 +25,31 @@ function animate() {
         c.strokeStyle = 'black'
         c.stroke()
         c.beginPath()
-        c.arc(x + radius - 2,y - 2,radius - radius*0.6,0,2*Math.PI)
+        //c.arc(x + radius - 2,y - 2,radius - radius*0.6,0,2*Math.PI)
         c.fillStyle = 'white'
         c.fill()
         x+= 2*radius
         }
+
+        x = 0
+        for (i = 0; i < 75;i++) {
+            if (timer - x*0.1 >= 0) {
+                y = starty + 30*Math.sin(0 - 0.4*(timer - x*0.1))
+            }else{
+                y = starty
+            }
+            c.beginPath()
+            c.arc(300 - x - radius - 0.6,y,radius,0,2*Math.PI)
+            c.fillStyle = 'blue'
+            c.fill()
+            c.strokeStyle = 'black'
+            c.stroke()
+            c.beginPath()
+            //c.arc(300 - x - radius - 2,y - 2,radius - radius*0.6,0,2*Math.PI)
+            c.fillStyle = 'white'
+            c.fill()
+            x+= 2*radius
+            }
 
         timer+= 0.03
 if (loop == true) {
