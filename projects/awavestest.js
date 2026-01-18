8dba63e9e0a74417d788eb26407a1d1e900221ee
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2026-01-18 13:48:22 -0300

    added bns (first part)

--- projects/awavestest.js
+++ projects/awavestest.js
@@ -17,6 +17,7 @@ const canv = document.getElementById("canvas")
 const c = canv.getContext('2d')
 
 intlimits = []
+velocities = []
 const xinitial = 50
 const yinitial = 75
 
@@ -25,31 +26,33 @@ for (let i = 0; i < contsnumber; i++){
    if (i < 0) {
       intlimits.push({xinf:i*radius*2, xsup: i*radius*2 + 2*radius, y: yinitial - yinitial,vel:0})
    }else{
-      intlimits.push({xinf: i*radius*2, xsup: i*radius*2 + 2*radius, y: 200*Math.sin(i*radius*2),vel:0})
+      intlimits.push({xinf: i*radius*2, xsup: i*radius*2 + 2*radius, y: 150*Math.sin(i*radius*2),vel:0})
+      velocities.push(1)
    }
 }
 
 
 ans = []
+bns = []
 for (let n = 1; n < modos; n++){
 let an = 0
-for (let i = 0; i < contsnumber; i++){
+let bn = 0
+for (let i = 0; i < contsnumber; i++){ // for each x interval
     // 2/L.y(x)∫(xinf - xsup)sin(nπx/L).dx
    // 2/L.y(x).-L/nπx.cos(nπx/l)
    // 2/L.y(x).(-L/nπx.cos(nπxsub/l) + L/nπ.cos(nπxinf/l)
    // 2/L.y(x).(-L/nπ(cos(nπxsub/L) - cos(nπxinf/L)))
-   console.log('y',intlimits[i].y,'xinferior', intlimits[i].xinf,'xsuperior', intlimits[i].xsup)
+   //console.log('y',intlimits[i].y,'xinferior', intlimits[i].xinf,'xsuperior', intlimits[i].xsup)
+   let wn2 = (n*Math.PI*v)/L
    an += (2/L)*intlimits[i].y*((-L/(n*Math.PI))*(Math.cos(n*Math.PI*intlimits[i].xsup/L) - Math.cos(n*Math.PI*intlimits[i].xinf/L)))
+   bn += (2/(wn2*L))*velocities[i]*((-L/(n*Math.PI))*(Math.cos(n*Math.PI*intlimits[i].xsup/L) - Math.cos(n*Math.PI*intlimits[i].xinf/L)))
 }
 ans.push(an)
+bns.push(bn)
 console.log(an)
-}
+}   
 
 
-a = 0 
-for (let n = 1; n < modos; n++){
-   a += 
-}
 animate()   
 
  b = 6*timer
@@ -70,7 +73,7 @@ for (let i = 0; i < contsnumber; i++){
    let k = (n*Math.PI*(intlimits[i].xinf + radius))/L
    let wn = (n*Math.PI*v)/L
    gamma = -0.09
-   intlimits[i].y += Math.exp(gamma*timer)*Math.sin(k) * ans[n-1] * Math.cos(wn * timer) //+ Math.sin(k) * bns[n - 1] * Math.sin(wn * timer);
+   intlimits[i].y += Math.exp(gamma*timer)*Math.sin(k) * (ans[n-1] * Math.cos(wn * timer) + bns[n-1] * Math.sin(wn * timer)); //+ Math.sin(k) * bns[n - 1] * Math.sin(wn * timer);
    //intlimits[i].exp = `sin(${k}).Ancos(${wn}.${timer}) + sin(${k}).Bnsin(${wn}.${timer})`
    //console.log(intlimits[i].exp)
     }     
@@ -87,6 +90,8 @@ requestAnimationFrame(animate)
 }
 }
 
-function timerset(n) {
-return n*timer
+function Timerset(n) {
+  this.el = function (n) {
+   return n*timer
+  }
 }
