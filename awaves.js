2b166dfa5cf45dd6960ae2c5e2f333a46b9be867
Ellen Kaori <akemitakihi@gmail.com>
2026-02-21 11:07:13 -0300

    case 2 -> uma exrtremidade fixa

--- awaves.js
+++ awaves.js
@@ -1,10 +1,11 @@
 let ans = []
 let bns = []
 let timer = 0
+let timer_nochange = 0 
 let lastTime = 0
 let intlimits = []
-const tension = 80
-const density = 0.05
+const tension = 70
+const density = 0.01
 const v = Math.sqrt(tension/density)
 const L = 250 
 const contsnumber = 121 // N >= 2M
@@ -16,6 +17,11 @@ const xinitial = 2
 const yinitial = 75
 const dot = 0
 const h0 = 20
+const a = 15
+const k = 2.23
+const res = true
+let Tp_n = 0
+let T_p_n = 0
 
 //const L = radius*2*contsnumber
 const modos = 120
@@ -35,10 +41,10 @@ for (let i = 0; i < contsnumber; i++){
    
   
    if (i == dot) {
-      intlimits.push({xinf:i*radius*2, xsup: i*radius*2 + 2*radius, y: h0,vel:0})
+      intlimits.push({xinf:i*radius*2, xsup: i*radius*2 + 2*radius, y: 0,vel:a*k})
       velocities.push(0)
    }else if (dot != contsnumber){
-      intlimits.push({xinf: i*radius*2, xsup: i*radius*2 + 2*radius, y: -10,vel:0})
+      intlimits.push({xinf: i*radius*2, xsup: i*radius*2 + 2*radius, y: -4,vel:0})
       velocities.push(0)
    }else{
       intlimits.push({xinf: i*radius*2, xsup: i*radius*2 + 2*radius, y: 0,vel:0})
@@ -64,6 +70,7 @@ function animate() {
     lastTime = currentTime;
     
     timer += dt
+    timer_nochange += dt
 
     c.clearRect(0, 0, 300, 150);
 
@@ -71,9 +78,9 @@ for (let i = 0; i < contsnumber; i++){
 
    x_center = intlimits[i].xinf + radius
    c.beginPath()
-   if (dot != 0) {
+   //if (i != dot) {
       c.arc(x_center + xinitial, intlimits[i].y + yinitial, radius, 0, Math.PI*2); // círculo completo
-   }
+   //}
    c.stroke(); // contorno
    if (i != dot) {
       c.fillStyle = 'red'
@@ -84,16 +91,18 @@ for (let i = 0; i < contsnumber; i++){
    c.fill()
    c.closePath()
    
+   // damping!
+   gamma = -0.2
    if (i != dot) {
-       intlimits[i].y = intlimits[0].y*(1 - x_center/L)
+      intlimits[i].y = Math.exp(gamma*timer) * (intlimits[0].y*(1 - x_center/L))
    }else{
-      // intlimits[i].y = a*Math.sin(c*timer_nochange)
+      intlimits[0].y = a*Math.sin(k*timer_nochange)
    }
   
    if (dot != 1) {
       vel = 0
    }else{
-      vel = 0 // a*c*Math.cos(c*timer_nochange)
+      vel = a*k*Math.cos(k*timer_nochange)
    }
 
    if (i != dot) {
@@ -101,26 +110,27 @@ for (let i = 0; i < contsnumber; i++){
 
    let kx = (n*Math.PI*x_center)/L
    let wn = (n*Math.PI*v)/L
-   gamma = -0.5
+   
    //intlimits[i].y += Math.exp(gamma*timer)*Math.sin(k) * (ans[n-1] * Math.cos(wn * timer) + bns[n-1] * Math.sin(wn * timer)); //+ Math.sin(k) * bns[n - 1] * Math.sin(wn * timer);
     //sin_medio = (cos(k*intlimits[i].xinf) - cos(k*intlimits[i].xsup)) / (k*(intlimits[i].xsup - intlimits[i].xinf))
 
-    tp_n = 0
-    t_p_n = 0
-    // k != wn:
-// tp_n = (2*a*k**2/(n*Math.PI*(wn2**2 - k**2)))*Math.sin(k*timer_nochange)
-// else:
-// tp_n = - (a*k/(2*n*Math.PI))*(timer_nochange)*Math.cos(k*(timer_nochange))
-// an += Tp,n(0) 
-// k != wn2::
-// t_p_n = (2*a*k**3/(n*Math.PI*(wn2**2 - k**2)))*Math.cos(k*timer_nochange)
-// else:
-// t_p_n = - (a*k/(2*n*Math.PI))*Math.cos(k*timer_nochange) + (a*k**2/(2*n*Math.PI))*(timer_nochange)*Math.sin(k*timer_nochange)
-// bn += T'p,n(0)
+   if (Math.abs(k - wn) > 1e-10 || res == false) {
+   Tp_n = (2*a*k**2/(n*Math.PI*(wn**2 - k**2)))*Math.sin(k*timer_nochange)
+   }else{
+   Tp_n = - (a*k/(2*n*Math.PI))*(timer)*Math.cos(k*(timer_nochange))
+   }
 
+  if (Math.abs(k - wn) > 1e-10 || res == false ) {
+   T_p_n = (2*a*k**3/(n*Math.PI*(wn**2 - k**2)))*Math.cos(k*timer_nochange)
+  }else{
+   T_p_n = - (a*k/(2*n*Math.PI))*Math.cos(k*timer_nochange) + (a*k**2/(2*n*Math.PI))*(timer_nochange)*Math.sin(k*timer_nochange)
+  }
+
+// Math.exp(gamma*timer)
     // com ressonãncia:
-    intlimits[i].y +=  Math.exp(gamma*timer) * Math.sin(kx) * (ans[n-1] * Math.cos(wn * timer) + bns[n-1] * Math.sin(wn * timer) + tp_n);
-    vel +=  Math.exp(gamma*timer) * Math.sin(kx) * (wn * ( bns[n-1] * Math.cos(wn * timer)  - ans[n-1] * Math.sin(wn * timer)) + t_p_n) //+ velocities[0]*(1 - x_center/L)
+    intlimits[i].y +=  Math.exp(gamma*timer) * (Math.sin(kx) * (ans[n-1] * Math.cos(wn * timer) + bns[n-1] * Math.sin(wn * timer) + Tp_n));
+    vel +=  Math.exp(gamma*timer)* (Math.sin(kx) * (wn * ( bns[n-1] * Math.cos(wn * timer)  - ans[n-1] * Math.sin(wn * timer)) + T_p_n) + 
+    velocities[0]*(1 - x_center/L))
    //intlimits[i].exp = `sin(${kx}).Ancos(${wn}.${timer}) + sin(${kx}).Bnsin(${wn}.${timer})`
    //console.log(intlimits[i].exp)
     }    
@@ -154,17 +164,19 @@ bns = []
 for (let n = 1; n < modos; n++){
 let wn2 = (n*Math.PI*v)/L
 let an = -(2*intlimits[0].y)/(n*Math.PI)
-// k != wn2:
-// Tp,n(0) = (2*a*k**2/(n*Math.PI*(wn2**2 - k**2)))*Math.sin(k*timer_nochange)
-// else:
-// Tp,n(0) = - (a*k/(2*n*Math.PI))*(timer_nochange)*Math.cos(k*(timer_nochange))
-// an += Tp,n(0) 
+if (Math.abs(k - wn2) > 1e-10 || res == false) {
+ Tp_n0 = (2*a*k**2/(n*Math.PI*(wn2**2 - k**2)))*Math.sin(k*timer_nochange)
+}else{
+ Tp_n0 = - (a*k/(2*n*Math.PI))*(timer_nochange)*Math.cos(k*(timer_nochange))
+}
+ an -= Tp_n0
 let bn = - (2*velocities[0])/(n*Math.PI)
-// k != wn2::
-// T'p,n(0) = (2*a*k**3/(n*Math.PI*(wn2**2 - k**2)))*Math.cos(k*timer_nochange)
-// else:
-// T'p,n(0) = - (a*k/(2*n*Math.PI))*Math.cos(k*timer_nochange) + (a*k**2/(2*n*Math.PI))*(timer_nochange)*Math.sin(k*timer_nochange)
-// bn += T'p,n(0)
+if (Math.abs(k - wn2) > 1e-10 || res == false) {
+ T_p_n0 = (2*a*k**3/(n*Math.PI*(wn2**2 - k**2)))*Math.cos(k*timer_nochange)
+}else{
+ T_p_n0 = - (a*k/(2*n*Math.PI))*Math.cos(k*timer_nochange) + (a*k**2/(2*n*Math.PI))*(timer_nochange)*Math.sin(k*timer_nochange)
+}
+ bn -= T_p_n0
 
 // FAZENDO A INTEGRAÇÃO POR PARTES
 for (let i = 0; i < contsnumber; i++){ // for each x interval
@@ -194,7 +206,7 @@ window.addEventListener('mousemove', function (event) {
 
 })
 
-setInterval(() => {timer = 0; update()}, 10);
+setInterval(() => {timer = 0; update()}, 6);
 
 /*
 function updatevelocities() {
