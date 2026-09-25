15edd1a19e8d07155689eca8b9a2d4ecade45a80
Ellen Kaori <92224126+LNKAMAKI@users.noreply.github.com>
2026-09-24 21:52:58 -0300

    debugging calculator

--- projects/calculadora.js
+++ projects/calculadora.js
@@ -10,7 +10,7 @@ window.addEventListener('keydown', (event)=> {
     if (String(event.key).search('[0-9]') != -1 || event.key.search('[\\.\\(\\)\\+\\-\\x\\/r*=Backspace]') != -1) {
     
         document.getElementById('put').style.justifyContent = 'end'
-        console.log('DIGITO: ' +event.key)
+        //console.log('DIGITO: ' +event.key)
         
     
         if (String(event.key).search('[=Backspace]') == -1) {
@@ -18,27 +18,27 @@ window.addEventListener('keydown', (event)=> {
         if (expression.length > 0) {
     
         if (String(event.key).search('[0-9]') != -1 ||String(event.key) == '.') {
-            console.log('NUMERO OU PONTO')
+            //console.log('NUMERO OU PONTO')
     
             if (ansd == true) {
                 ansd = false
             }
            if (String(expression[expression.length - 1]).search('[0-9]') != -1 || String(expression[expression.length - 1]) == '.' || juntar == true) {
-               console.log(juntar)
-            console.log(expression[expression.length - 1] + ' é umevent.keyúmero ou ponto')
+               //console.log(juntar)
+            //console.log(expression[expression.length - 1] + ' é umevent.keyúmero ou ponto')
             expression[position] +=String(event.key)
     
             juntar = false
            }else{
-               console.log(expression[expression.length - 1] + 'event.keyão é umevent.keyúmero ou ponto')
+               //console.log(expression[expression.length - 1] + 'event.keyão é umevent.keyúmero ou ponto')
                position++
             expression[position] =String(event.key)
            }
         }else{
-            console.log('EEEEEEEEEEEEI')
-            console.log(expression[expression.length - 1], String(expression[expression.length - 1]).search('[\\x\\*\\+\\(\\/]') != -1)
+            //console.log('EEEEEEEEEEEEI')
+            //console.log(expression[expression.length - 1], String(expression[expression.length - 1]).search('[\\x\\*\\+\\(\\/]') != -1)
             if (String(event.key) == '-' && String(expression[expression.length - 1]).search('[\\x\\*\\+\\(\\/]') != -1 ||String(event.key) == '-' && expression[expression.length - 1] == '-') {
-                console.log('TEMOS UM PROBLEMÃO')
+                //console.log('TEMOS UM PROBLEMÃO')
                 juntar = true
             }
             if (expression.length > 0) {
@@ -49,12 +49,12 @@ window.addEventListener('keydown', (event)=> {
         }
       
     }else{
-        console.log('LISTA VAZIA')
+        //console.log('LISTA VAZIA')
         if (String(event.key).search('[0-9]') != -1 ||String(event.key) == '.' ||String(event.key) == '(' ||String(event.key) == '-' ||String(event.key) == '√')  {
        expression.push(String(event.key))
         }
        if (event.key == '-') {
-        console.log('OOOPS')
+        //console.log('OOOPS')
         juntar = true
        }
       
@@ -113,19 +113,19 @@ window.addEventListener('keydown', (event)=> {
                   //  document.getElementById('ans').innerText = ''
                 }
             }else{
-                console.log('ARE YOU KIDDING ME?')
+                //console.log('ARE YOU KIDDING ME?')
                 position = 0
               //  document.getElementById('put').innerText = '777777'
                 error = false
             }
                 if (expression.length == 0) {
-                    console.log('É ZEEEEEEEEEEEEEEROOOOOOOOOOOOOO')
+                    //console.log('É ZEEEEEEEEEEEEEEROOOOOOOOOOOOOO')
                     ansd = false
                 }
             }else{
                // console.log('=')
                
-        
+        // Resolvendo o trem dos parênteses
         degs = []
         parents = 0
         for (e = 0; e < expression.length; e++) {
@@ -147,7 +147,7 @@ window.addEventListener('keydown', (event)=> {
         
                 calcular = ''
                 for (n = e + 1; stop2 == false && n < expression.length; n++) {
-                    //console.log(continuar)
+                    console.log(continuar)
                     //console.log(continuar == true)
                     console.log(expression[n])
                     if (expression[n] == '(') {
@@ -235,7 +235,24 @@ window.addEventListener('keydown', (event)=> {
     }
 })
 
-
+expression = ['(','(','9','+','3',')','x','(','2','-','1',')',')','x','2']
+expression = ['(', '10', '-', '(', '4', '-', '2', ')', ')', 'x', '5']
+console.log(2)
+console.log(2)
+console.log(2)
+console.log(2)
+console.log(2)
+console.log(2)
+console.log(2)
+console.log(2)
+console.log(2)
+console.log(2)
+console.log(2)
+console.log(2)
+console.log(2)
+console.log(2)
+console.log(2)
+resolverParênteses()
 
 function ok() {
     document.getElementById('inst').style.animation = 'fall2 1s forwards'
@@ -249,7 +266,7 @@ anstxt = ''
 error = false
 function inserir(n) {
     document.getElementById('put').style.justifyContent = 'end'
-    console.log('DIGITO: ' + n)
+    //console.log('DIGITO: ' + n)
     
 
     if (String(n).search('[=CE]') == -1) {
@@ -257,27 +274,27 @@ function inserir(n) {
     if (expression.length > 0) {
 
     if (String(n).search('[0-9]') != -1 || String(n) == '.') {
-        console.log('NUMERO OU PONTO')
+        //console.log('NUMERO OU PONTO')
 
         if (ansd == true) {
             ansd = false
         }
        if (String(expression[expression.length - 1]).search('[0-9]') != -1 || String(expression[expression.length - 1]) == '.' || juntar == true) {
-           console.log(juntar)
-        console.log(expression[expression.length - 1] + ' é um número ou ponto')
+           //console.log(juntar)
+        //console.log(expression[expression.length - 1] + ' é um número ou ponto')
         expression[position] += String(n)
 
         juntar = false
        }else{
-           console.log(expression[expression.length - 1] + ' não é um número ou ponto')
+           //console.log(expression[expression.length - 1] + ' não é um número ou ponto')
            position++
         expression[position] = String(n)
        }
     }else{
-        console.log('EEEEEEEEEEEEI')
-        console.log(expression[expression.length - 1], String(expression[expression.length - 1]).search('[\\x\\*\\+\\(\\/]') != -1)
+        //console.log('EEEEEEEEEEEEI')
+        //console.log(expression[expression.length - 1], String(expression[expression.length - 1]).search('[\\x\\*\\+\\(\\/]') != -1)
         if (String(n) == '-' && String(expression[expression.length - 1]).search('[\\x\\*\\+\\(\\/]') != -1 || String(n) == '-' && expression[expression.length - 1] == '-') {
-            console.log('TEMOS UM PROBLEMÃO')
+            //console.log('TEMOS UM PROBLEMÃO')
             juntar = true
         }
         if (expression.length > 0) {
@@ -288,12 +305,12 @@ function inserir(n) {
     }
   
 }else{
-    console.log('LISTA VAZIA')
+    //console.log('LISTA VAZIA')
     if (String(n).search('[0-9]') != -1 || String(n) == '.' || String(n) == '(' || String(n) == '-' || String(n) == '√')  {
    expression.push(String(n))
     }
    if (n == '-') {
-    console.log('OOOPS')
+    //console.log('OOOPS')
     juntar = true
    }
   
@@ -328,23 +345,23 @@ console.log(expression)
            if (error == false /*&& String(document.getElementById('put').innerText).search('Infinity') == -1*/) {
             if (String(expression[expression.length - 1]).length == 1) {
                 console.log('AAAAAAAA')
-               // console.log(expression[expression.length - 1] + ' tem tamanho 1')
+                console.log(expression[expression.length - 1] + ' tem tamanho 1')
                 expression.splice(expression.length - 1, 1)
-                //console.log(expression)
+                console.log(expression)
 
                 if (position != 0) {
                     position--
                     }
             }else{
                 slicednum = ''
-               // console.log(expression[expression.length - 1] + ' tem tamanho maior que 1') 
+                console.log(expression[expression.length - 1] + ' tem tamanho maior que 1') 
                 for (na = 0; na <= Number(String(expression[expression.length - 1]).length) - 2; na++) {
-                    //console.log(na, String(expression[expression.length - 1])[na])
+                    console.log(na, String(expression[expression.length - 1])[na])
                     slicednum +=  String(expression[expression.length - 1])[na]
                 }
-                //console.log(slicednum)
+                console.log(slicednum)
                 expression[expression.length - 1] = slicednum
-                //console.log(expression)
+                console.log(expression)
 
             }
             console.log(expression)
@@ -445,6 +462,7 @@ console.log(expression)
             
            if (expression.length != 0 && falta == false) {
                anstxt = txt
+               console.warn('fazer conta recebe:',anstxt)
             fazerConta(anstxt) 
             position = 0    
            ansd = true
@@ -479,6 +497,7 @@ console.log(expression)
 //FAZER CONTA
 function fazerConta(anterior) {
 
+    console.log('bora fazer a conta')
         if(0 == 0) {// falta == false
     //Primeiro raizes e potências
     for (n = 0; n < expression.length; n++) {
@@ -675,41 +694,140 @@ if (expression.length > 1) {
 //document.getElementById('put').style.justifyContent = 'start'
 
 
-   rem = String(document.getElementById('ans').innerText)
+   //rem = String(document.getElementById('ans').innerText)
   
-    document.getElementById('selc').removeChild(document.getElementById('put'))
-    op = document.createElement('option')
-    op.id = 'put'
-    document.getElementById('selc').appendChild(op)
-    op.style.animation = 'goup 0.3s'
+    //document.getElementById('selc').removeChild(document.getElementById('put'))
+    //op = document.createElement('option')
+   //op.id = 'put'
+    //document.getElementById('selc').appendChild(op)
+    //op.style.animation = 'goup 0.3s'
 
-    document.getElementById('selc').removeChild(document.getElementById('ans'))
-    op2 = document.createElement('option')
-    op2.id = 'ans'
+    //document.getElementById('selc').removeChild(document.getElementById('ans'))
+    //op2 = document.createElement('option')
+    //op2.id = 'ans'
     
 
-    document.getElementById('selc').appendChild(op2)
-    op2.style.animation = 'goup2 0.3s'
+    //document.getElementById('selc').appendChild(op2)
+    //op2.style.animation = 'goup2 0.3s'
 
     if (expression.indexOf('NaN') == -1 && expression.length == 1) {
-    document.getElementById('put').innerText = expression
+    //document.getElementById('put').innerText = expression
     if (expression[0].length > 17) {
-        document.getElementById('put').style.justifyContent = 'start'
+        //document.getElementById('put').style.justifyContent = 'start'
     }
 }else{
     console.log('what?')
     expression = []
-    document.getElementById('put').innerText = 'Error'
+    //document.getElementById('put').innerText = 'Error'
     error = true
     
 }
 
 if (ansd == false && error == false) {
-    op2.innerText = `${anterior} =`
+    //op2.innerText = `${anterior} =`
 }else{
-    op2.innerText = rem
-    anstxt = rem
+    //op2.innerText = rem
+    //anstxt = rem
 }
+
+return expression
         
 }
+}
+
+function resolverParênteses() {
+    degs = []
+    parents = 0
+        for (e = 0; e < expression.length; e++) {
+        
+            console.log('NUMBER: ' + expression[e])
+            
+        
+            if (expression[e] == '(') {
+                continuar = false
+                stop2 = false
+                pares = 0
+        
+                console.log('--------PARÊNTESES--------')
+                degs.push({})
+                console.log(degs)
+                degs[parents].start = e
+               
+                console.log('DEEEEEEGS[E] = ' + degs[parents])
+        
+                calcular = ''
+                for (n = e + 1; stop2 == false && n < expression.length; n++) {
+                    console.log(continuar)
+                    //console.log(continuar == true)
+                    console.log(expression[n])
+                    if (expression[n] == '(') {
+                        console.log('Ohevent.keyo')
+                        console.log(pares)
+                        pares++
+                        console.log('PARES: ' + pares)
+                        continuar = true
+                        degs[parents].par = pares
+                        console.log('DEGS PARES---------: ' + degs[parents].par)
+        
+                    }else if (continuar == true && expression[n] == ')'){ 
+                            console.log('TRUE')
+                          
+                            if (pares == 1) {
+                                continuar = false
+                            }
+                            pares--
+                            console.log('PARES: ' + pares)
+        
+                    }else if (continuar == false && expression[n] == ')') {
+        
+                        console.log(degs[parents])
+                        if (degs[parents].par == undefined) {
+                           console.log('TEM ALGO DE ERRADO')
+                            degs[parents].par = 0
+                        }
+                        console.log('MAIS')
+                        console.log('PARES: ' + pares)
+                        console.log('stop2: ' + stop2)
+                        stop2 = true
+                        console.log('stop2: ' + stop2)
+                        console.log('abre parenteses: ' + e + '    fecha parenteses: ', n )
+        
+                        degs[parents].end = n
+                    }
+                }
+                console.log('STOP2: ' + stop2)
+                console.log('PARES: ', pares)
+                
+                console.log('EXPRESSION!!!!!!!!!!!!: ' + expression)
+                parents++
+            }
+        } 
+                falta = false
+                if (degs.length > 0) {
+                for (i = 0; i < degs.length && false == false; i++) {
+                    console.log(degs[i].end)
+                    if (degs[i].end == undefined) {
+                        falta = true
+                    }
+                }
+            }else if(expression.indexOf(')') != -1){
+                falta = true
+            }
+                
+               if (expression.length != 0 && falta == false) {
+
+                txt = ''
+                for (b = 0; b < expression.length; b++) {
+                    //console.log(expression[b])
+                    txt+= ` ${expression[b]}`
+                }
+
+                   anstxt = txt
+                   console.warn('fazer conta de ',anstxt)
+                console.log('igual a: ',fazerConta(anstxt)) 
+                position = 0    
+               ansd = true
+    
+                
+               }
 }
